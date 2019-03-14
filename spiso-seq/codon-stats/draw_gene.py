9b3e993d0fc3d05311b8fb41c72faaceb89145cb
Andrey Prjibelski <andrewprzh@gmail.com>
2019-03-14 04:01:50 +0300

    contig drawing

--- spiso-seq/codon-stats/draw_gene.py
+++ spiso-seq/codon-stats/draw_gene.py
@@ -3,51 +3,105 @@ import sys
 from plot_common import *
 
 
-if len(sys.argv) < 3:
-    print("Usage: " + sys.argv[0] + " <gene coord file> <gene id> [output figure name = gene_id]")
+if len(sys.argv) < 4:
+    print("Usage: " + sys.argv[0] + " <gene id> <gene coord file> <contig coord file> [output figure name = gene_id]")
     exit(0)
 
-inf = open(sys.argv[1])
-gene_id = sys.argv[2]
-out_name = sys.argv[3] if len(sys.argv) > 3 else gene_id
+gene_id = sys.argv[1]
+gene_coord_file = open(sys.argv[2])
+contig_coord_file = open(sys.argv[3])
+out_name = sys.argv[4] if len(sys.argv) > 4 else gene_id
 
 found_gene = False
-l = inf.readline()
-while l and not found_gene:
-    if not l.startswith("gene\t" + gene_id):
-        l = inf.readline()
-        continue
+lg = gene_coord_file.readline()
+while lg and not found_gene:
+    if lg.startswith("gene\t" + gene_id):
+        found_gene = True
+        break
+    lg = gene_coord_file.readline()
 
-    tokens = l.strip().split('\t')
+found_contigs = False
+lc = contig_coord_file.readline()
+while lc and not found_contigs:
+    if lc.startswith("gene\t" + gene_id):
+        found_contigs = True
+        break
+    lc = contig_coord_file.readline()
+
+
+if not found_gene and not found_contigs:
+    print("Gene was not detected in both files")
+    exit(0)
+
+total_contigs = 0
+total_transcripts = 0
+gene_coords = (0, 0)
+if found_contigs:
+    tokens = lc.strip().split('\t')
+    if len(tokens) != 7:
+        print("Wrong format")
+        exit(0)
+    gene_coords = (int(tokens[4]), int(tokens[5]))
+    total_contigs = int(tokens[6])
+
+if found_gene:
+    tokens = lg.strip().split('\t')
     if len(tokens) != 8:
         print("Wrong format")
-        break
-
-    found_gene = True
+        exit(0)
     gene_coords = (int(tokens[4]), int(tokens[5]))
     total_transcripts = int(tokens[6])
-    drawer = GeneDrawer(total_transcripts, gene_coords, out_name)
-    drawer.draw_gene(gene_coords)
 
-    l = inf.readline()
-    transcript_num = 0
-    while l and not l.startswith("gene"):
-        tokens = l.strip().split('\t')
-        if len(tokens) != 3:
-            print("Wrong format")
-            break
-        coords = (int(tokens[1]), int(tokens[2]))
-        if tokens[0] == "transcript":
-            transcript_num += 1
-            drawer.draw_transcript(coords, transcript_num)
-        elif tokens[0] == "CDS":
-            drawer.draw_exon(coords, transcript_num)        
-        elif tokens[0] == "start_codon":
-            drawer.draw_start_codon(coords, transcript_num)
+
+drawer = GeneDrawer(total_transcripts, total_contigs, gene_coords, out_name)
+drawer.draw_gene(gene_coords)
+
+lg = gene_coord_file.readline()
+transcript_num = 0
+while lg and not lg.startswith("gene"):
+    tokens = lg.strip().split('\t')
+    if len(tokens) < 3:
+        print("Wrong format")
+        break
+    coords = (int(tokens[1]), int(tokens[2]))
+    if tokens[0] == "transcript":
+        transcript_num += 1
+        drawer.draw_transcript(coords, transcript_num)
+    elif tokens[0] == "CDS":
+        drawer.draw_exon(coords, transcript_num)        
+    elif tokens[0] == "start_codon":
+        drawer.draw_start_codon(coords, transcript_num, -1)
+        drawer.draw_start_codon(coords)                
+    elif tokens[0] == "stop_codon":
+        drawer.draw_stop_codon(coords, transcript_num, -1)        
+        drawer.draw_stop_codon(coords)   
+    lg = gene_coord_file.readline()
+
+lc = contig_coord_file.readline()
+contig_num = 0
+cov = 1
+while lc and not lc.startswith("gene"):
+    tokens = lc.strip().split('\t')
+    if len(tokens) < 3:
+        print("Wrong format")
+        break
+    coords = (int(tokens[1]), int(tokens[2]))
+    if tokens[0] == "contig":
+        contig_num += 1
+        cov = int(tokens[3])
+        drawer.draw_contig(coords, contig_num)
+    elif tokens[0] == "block":
+        drawer.draw_block(coords, contig_num, cov)        
+    elif tokens[0] == "start_codon":
+        if contig_num == 0:
             drawer.draw_start_codon(coords)                
-        elif tokens[0] == "stop_codon":
-            drawer.draw_stop_codon(coords, transcript_num)        
+        else:
+            drawer.draw_start_codon(coords, contig_num, 1)
+    elif tokens[0] == "stop_codon":
+        if contig_num == 0:
             drawer.draw_stop_codon(coords)   
-        l = inf.readline()
+        else:
+            drawer.draw_stop_codon(coords, contig_num, 1)        
+    lc = contig_coord_file.readline()
     
 
--- spiso-seq/codon-stats/plot_common.py
+++ spiso-seq/codon-stats/plot_common.py
@@ -7,24 +7,30 @@ import math
 
 class GeneDrawer:
     SPACE_BETWEEN_LEVELS = 10
+    LOW_SPACE_BETWEEN_LEVELS = 8
     BASE_LEVEL = 3
-    XMARGIN = 50
+    XMARGIN = 100
     YMARGIN = 50
     CODON_SIZE = 3
 
-    def __init__(self, transcript_num, gene_coords, fname):
+    def __init__(self, transcript_num, contig_num, gene_coords, fname):
         self.GENOME_START_POS = gene_coords[0]
         self.GENOME_END_POS = gene_coords[1]
         self.TOTAL_GENES = transcript_num
+        self.TOTAL_CONTIGS = contig_num
 
         self.CANVAS_WIDTH = min(2000, self.GENOME_END_POS - self.GENOME_START_POS)
-        self.CANVAS_HEIGHT = max(300, self.SPACE_BETWEEN_LEVELS * (self.TOTAL_GENES + self.BASE_LEVEL))
+        self.GENE_CANVAS_HEIGHT = max(150, self.SPACE_BETWEEN_LEVELS * (self.TOTAL_GENES + self.BASE_LEVEL))
+        self.CONTIG_CANVAS_HEIGHT = max(150, self.LOW_SPACE_BETWEEN_LEVELS * (self.TOTAL_CONTIGS + self.BASE_LEVEL))
+        self.CANVAS_HEIGHT = self.CONTIG_CANVAS_HEIGHT + self.GENE_CANVAS_HEIGHT
+        self.GENE_LEVEL = self.GENE_CANVAS_HEIGHT
 
         self.WIDTH = self.CANVAS_WIDTH + 2 * self.XMARGIN
         self.HEIGHT = self.CANVAS_HEIGHT + 2 * self.YMARGIN
 
         self.XSCALE = float(self.CANVAS_WIDTH) / float(self.GENOME_END_POS - self.GENOME_START_POS)
-        self.YSCALE = float(self.CANVAS_HEIGHT) / float(self.TOTAL_GENES + self.BASE_LEVEL)
+        self.GENE_YSCALE = float(self.GENE_CANVAS_HEIGHT) / float(self.TOTAL_GENES + self.BASE_LEVEL)
+        self.CONTIG_YSCALE = float(self.CONTIG_CANVAS_HEIGHT) / float(self.TOTAL_CONTIGS + self.BASE_LEVEL)
 
         self.surface = cairo.SVGSurface(fname + ".svg", self.WIDTH, self.HEIGHT)
         self.context = cairo.Context(self.surface)
@@ -37,7 +43,11 @@ class GeneDrawer:
     def draw_element(self, genome_coords, level):
         x1 = self.XMARGIN + (genome_coords[0] - self.GENOME_START_POS)  * self.XSCALE
         x2 = self.XMARGIN + (genome_coords[1] - self.GENOME_START_POS)  * self.XSCALE
-        y = self.YMARGIN + level * self.YSCALE
+        y = 0
+        if level < 0:
+            y = self.YMARGIN + self.GENE_LEVEL + level * self.GENE_YSCALE
+        else:
+            y = self.YMARGIN + self.GENE_LEVEL + level * self.CONTIG_YSCALE
         self.context.move_to(x1, y)
         self.context.line_to(x2, y)
         self.context.stroke()
@@ -46,7 +56,12 @@ class GeneDrawer:
 
     def draw_codon(self, genome_coords, level):
         x = self.XMARGIN + (genome_coords[0] + genome_coords[1] - 2 * self.GENOME_START_POS) * self.XSCALE / 2
-        y = self.YMARGIN + level * self.YSCALE
+        y = 0
+        if level < 0:
+            y = self.YMARGIN + self.GENE_LEVEL + level * self.GENE_YSCALE
+        else:
+            y = self.YMARGIN + self.GENE_LEVEL + level * self.CONTIG_YSCALE
+
         self.context.arc(x, y, self.CODON_SIZE, 0, 2 * math.pi)
         self.context.fill()
         self.context.stroke()
@@ -61,26 +76,34 @@ class GeneDrawer:
     def draw_transcript(self, coords, transcript_num):
         self.context.set_source_rgb(0.5, 0.5, 0.5)
         self.context.set_line_width(1)
-        self.draw_element(coords, transcript_num + self.BASE_LEVEL)
+        self.draw_element(coords, - transcript_num - self.BASE_LEVEL)
 
 
     def draw_exon(self, coords, transcript_num):
         self.context.set_source_rgb(0.1, 0.1, 0.9)
         self.context.set_line_width(3)
-        self.draw_element(coords, transcript_num + self.BASE_LEVEL)
+        self.draw_element(coords, - transcript_num - self.BASE_LEVEL)
+
+    def draw_contig(self, coords, contig_num):
+        self.context.set_source_rgb(0.5, 0.5, 0.5)
+        self.context.set_line_width(1)
+        self.draw_element(coords, contig_num + self.BASE_LEVEL)
+
+    def draw_block(self, coords, contig_num, coverage = 1):
+        self.context.set_source_rgb(0.1, 0.1, 0.9)
+        self.context.set_line_width(min(6, 3 + math.log(coverage, 4)))
+        self.draw_element(coords, contig_num + self.BASE_LEVEL)
 
 
-
-    #transcript_num = 0 for reference drawing
-    def draw_start_codon(self, coords, transcript_num = -1):
+    #level_num = transcript / contig num, level_sign = 0 for gene leve, -1 for transcript, +1 for contig
+    def draw_start_codon(self, coords, level_num = 0, level_sign = 0):
         self.context.set_source_rgb(0.1, 0.9, 0.1)
-        self.draw_codon(coords, transcript_num + (1 if transcript_num == -1 else self.BASE_LEVEL))
+        self.draw_codon(coords, level_sign * (level_num + self.BASE_LEVEL))
 
-
-    #transcript_num = -self.BASE_LEVEL for reference drawing
-    def draw_stop_codon(self, coords, transcript_num = -1):
+    #level_num = transcript / contig num, level_sign = 0 for gene leve, -1 for transcript, +1 for contig
+    def draw_stop_codon(self, coords, level_num = 0, level_sign = 0):
         self.context.set_source_rgb(0.8, 0.1, 0.1)
-        self.draw_codon(coords, transcript_num + (1 if transcript_num == -1 else self.BASE_LEVEL))
+        self.draw_codon(coords, level_sign * (level_num + self.BASE_LEVEL))
 
     def draw_coords(self):
         pass
