f834f7c1ea761c4013297dd233745b350c7f55f5
Andrey Prjibelski <andrewprzh@gmail.com>
2019-11-06 21:30:48 +0300

    refactored preprocessing script

--- spiso-seq/preprocessing/common.py
+++ spiso-seq/preprocessing/common.py
@@ -1,72 +1,112 @@
+############################################################################
+# Copyright (c) 2019 Saint Petersburg State University
+# # All Rights Reserved
+# See file LICENSE for details.
+############################################################################
+
 import os
 import sys
 from Bio import SeqIO
 
 
-def get_spades_barcode(contig_id):
-    delimeter = "_barcodeIDs_"
-    tokens = contig_id.split(delimeter)
+def get_barcodes(contig_id, delim1, delim2):
+    tokens = contig_id.split(delim1)
     if len(tokens) != 2:
         print("Wrong fromat " + contig_id)
 
-    return tokens[0], tokens[1].strip() if len(tokens) > 1 else ""
-    
+    return tokens[0], tokens[1].strip().split(delim2) if len(tokens) > 1 else []
 
-def convert_fasta_with_barcodes(contigs_file, output_dir, barcode_from_id_function, max_len = 100000):
+
+def convert_fasta_with_barcodes(inf, args):
+    contigs_file = inf
     contigs_name, ext = os.path.splitext(contigs_file.split('/')[-1])
-    short_id_contigs_name = os.path.join(output_dir, contigs_name + "_short_ids.fasta")
-    map_file_name = os.path.join(output_dir, contigs_name + "_map.txt")
+    short_id_contigs_name = os.path.join(args.output_prefix, contigs_name + "_short_ids.fasta")
+    map_file_name = os.path.join(args.output_prefix, contigs_name + "_map.txt")
     mapf = open(map_file_name, "w")
 
     new_fasta = []
     for record in SeqIO.parse(contigs_file, "fasta"):
-        if len(record.seq) > max_len:
+        if len(record.seq) > args.max_len:
             continue
         name = record.id
         num = int(name.split('_')[1])
         if num % 2 == 1:
             continue
-        record.id, bc = barcode_from_id_function(name)
+        record.id, bc = get_barcodes(name, args.delim, args.delim2)
         new_fasta.append(record)
-        mapf.write(record.id + "_barcodeIDs_" + bc + "\n")
+        mapf.write(record.id + "_barcodeIDs_" + ",".join(bc) + "\n")
 
     mapf.close()
     SeqIO.write(new_fasta, short_id_contigs_name, "fasta")
     return contigs_name, short_id_contigs_name
 
 
-def align_fasta(output_dir, contigs_name, short_id_contigs_name, index, method = "gmap"):
-    alignment_name = os.path.join(output_dir, contigs_name)
+
+def align_fasta(contigs_infile, args, out_name = ""):
+    method = args.aligner
+    contigs_name, ext = os.path.splitext(contigs_infile.split('/')[-1])
+    if out_name == "":
+        out_name = contigs_name
+
+    alignment_name = os.path.join(args.output_prefix, out_name)
     alignment_sam_path = alignment_name + '.sam'
+    alignment_bam_path = alignment_name + '.bam'
+
     if method.lower() == "gmap":
         gmap_path = '/Bmo/prjbel/tools/gmap-2019-06-10/src/gmap'
-        command = gmap_path + ' -D ./  -d {ref_index_name} {transcripts} --format=samse -t 16 -O > {alignment_out} '.format(ref_index_name=index, transcripts=short_id_contigs_name, alignment_out=alignment_sam_path)
+        command = gmap_path + ' -D ./  -d {ref_index_name} {transcripts} --format=samse -t {threads} -O ' \
+                              '> {alignment_out} '.format(ref_index_name=args.index,
+                                                          transcripts=contigs_infile,
+                                                          threads=str(args.threads),
+                                                          alignment_out=alignment_sam_path)
         exit_code = os.system(command)
 
         if exit_code != 0:
             print("GMAP finished with errors")
 
+        os.system('samtools sort -o ' + alignment_bam_path + ' ' + alignment_sam_path)
+
     elif method.lower() == "star":
         star_path = '/Bmo/prjbel/tools/STAR-2.7.0f/bin/Linux_x86_64/STARlong'
-        #Simple 
-        #command = '{star} --runThreadN 16 --genomeDir {ref_index_name}  --readFilesIn {transcripts}  --outSAMtype SAM  --outFileNamePrefix {alignment_out}'.format(star=star_path, ref_index_name=star_index, transcripts=short_id_contigs_name, alignment_out=alignment_sam_path)
-        #Hagen options
-        command = '{star} --runThreadN 6 --genomeDir {ref_index_name}  --readFilesIn {transcripts}  --outSAMtype SAM --outSAMattributes NH HI NM MD --outFilterMultimapScoreRange 1 \
-                   --outFilterMismatchNmax 2000 --scoreGapNoncan -20 --scoreGapGCAG -4 --scoreGapATAC -8 --scoreDelOpen -1 --scoreDelBase -1 --scoreInsOpen -1 --scoreInsBase -1 \
-                   --alignEndsType Local --seedSearchStartLmax 50 --seedPerReadNmax 1000000 --seedPerWindowNmax 1000 --alignTranscriptsPerReadNmax 100000 --alignTranscriptsPerWindowNmax 10000 \
-                   --outFileNamePrefix {alignment_out}'.format(star=star_path, ref_index_name=index, transcripts=short_id_contigs_name, alignment_out=alignment_name)
+        zcat_option = " --readFilesCommand zcat " if ext.endswith('gz') else ""
+
+        # Simple
+        # command = '{star} --runThreadN 16 --genomeDir {ref_index_name}  --readFilesIn {transcripts}  --outSAMtype SAM
+        #  --outFileNamePrefix {alignment_out}'.format(star=star_path, ref_index_name=star_index, transcripts=short_id_contigs_name, alignment_out=alignment_sam_path)
+        # Hagen options
+        command = '{star} {zcat} --runThreadN {threads} --genomeDir {ref_index_name}  --readFilesIn {transcripts}  ' \
+                  '--outSAMtype BAM SortedByCoordinate --outSAMattributes NH HI NM MD --outFilterMultimapScoreRange 1 \
+                   --outFilterMismatchNmax 2000 --scoreGapNoncan -20 --scoreGapGCAG -4 --scoreGapATAC -8 --scoreDelOpen ' \
+                  '-1 --scoreDelBase -1 --scoreInsOpen -1 --scoreInsBase -1 \
+                   --alignEndsType Local --seedSearchStartLmax 50 --seedPerReadNmax 1000000 --seedPerWindowNmax 1000 ' \
+                  '--alignTranscriptsPerReadNmax 100000 --alignTranscriptsPerWindowNmax 10000 \
+                   --outFileNamePrefix {alignment_out}'.format(star=star_path,
+                                                               zcat=zcat_option,
+                                                               threads=str(args.threads),
+                                                               ref_index_name=args.index,
+                                                               transcripts=contigs_infile,
+                                                               alignment_out=alignment_name)
 
         exit_code = os.system(command)
-        os.system('mv ' + alignment_name + 'Aligned.out.sam ' + alignment_sam_path)
+        os.system('mv ' + alignment_name + 'Aligned.out.sam ' + alignment_bam_path)
 
         if exit_code != 0:
             print("STAR finished with errors")
+
+    elif method.lower == "minimap":
+        minimap_path = '/Bmo/prjbel/tools/minimap2-2.8_x64-linux/minimap2'
+        command =  minimap_path+ ' {ref_index_name} {transcripts} -a -x splice -t {threads} ' \
+                              '> {alignment_out} '.format(ref_index_name=args.index,
+                                                          transcripts=contigs_infile,
+                                                          threads=str(args.threads),
+                                                          alignment_out=alignment_sam_path)
+        exit_code = os.system(command)
+        if exit_code != 0:
+            print("minimap finished with errors")
 
     else:
         print("Method " + method + " is not supported")
         return 
 
-    alignment_bam_path = os.path.join(output_dir, contigs_name + '.bam')
-    os.system('samtools sort -o ' + alignment_bam_path + ' ' + alignment_sam_path)
     os.system('samtools index ' + alignment_bam_path)
 
--- spiso-seq/preprocessing/preprocess.py
+++ spiso-seq/preprocessing/preprocess.py
@@ -1,18 +1,84 @@
+############################################################################
+# Copyright (c) 2019 Saint Petersburg State University
+# # All Rights Reserved
+# See file LICENSE for details.
+############################################################################
+
 import os
 import sys
+from traceback import print_exc
+import argparse
+
+from Bio import SeqIO
 
 from common import *
 
-if len(sys.argv) < 4:
-    print("Usage: " + sys.argv[0] + " <FASTA file> <GMAP index> <output dir>")
-    exit(0)
+DATA_TYPES = {'assembly' : 'gmap', '10x_assembly' : 'gmap', 'isoseq' : 'minimap', 'ccs' : 'star', 'ont' : 'minimap'}
+ALIGNERS = set(['minimap', 'gmap', 'star'])
 
-contigs_file = sys.argv[1]
-gmap_index = sys.argv[2]
-output_dir = sys.argv[3]
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class = argparse.RawDescriptionHelpFormatter,
+                                     description="A simple script for spliced mapping of long reads/contigs using gmap/star/minimap2. "
+                                                 "Uses slightly tuned set of parameters for each data type. "
+                                                 "Correctly processes contigs obtained from 10x RNA data unsing rnaSPAdes.\n"
+                                                 "Available data types: assembly, 10x_assembly (gmap will be used), "
+                                                 "isoseq, ont (minimap will be used) and ccs (STAR long will be used). ")
 
-contigs_name, short_id_contigs_name = convert_fasta_with_barcodes(contigs_file, output_dir, get_spades_barcode)
+    required_group = parser.add_argument_group('required parameters')
+    required_group.add_argument('--input', '-i', dest='fasta_files', nargs='+', help='list of FASTA / FASTQ files')
+    required_group.add_argument("--index", "-x", help="reference genome index; should be consistent with selected aligner", type=str)
+    required_group.add_argument("--data_type", "-d", help="assembly/10x_assembly/isoseq/ccs/ont", type=str)
+    required_group.add_argument("--output_prefix", "-o", help="output prefix", type=str)
+    optional_group = parser.add_argument_group('optional parameters')
+    optional_group.add_argument("--aligner", "-a", help="force to use aligner auto/minimap/star/gmap "
+                                                        "(selected automatically based on data type)", type=str)
+    optional_group.add_argument("--delim", help="delimeter between read id and barcode for 10x assembly "
+                                                      "(default:_barcodeIDs_) ", type=str, default='_barcodeIDs_')
+    optional_group.add_argument("--delim2", help="delimeter between barcodes "
+                                                      "(default:comma) ", type=str, default=',')
+    optional_group.add_argument("--threads", "-t", help="threads for aligner (16)", type=int, default=16)
+    optional_group.add_argument("--max_len", help="skip contigs longer than (0), valid for 10x assembly only", type=int, default=16)
 
-align_fasta(output_dir, contigs_name, short_id_contigs_name, gmap_index)
+
+    args = parser.parse_args()
+
+    if args.fasta_files is None or len(args.fasta_files) == 0 or args.index is None or args.output_prefix is None:
+        print("ERROR: not enough parameters")
+        parser.print_help()
+        exit(-1)
+
+    if args.data_type is None or args.data_type not in DATA_TYPES:
+        print("ERROR: specify data type among assembly/10x_assembly/isoseq/ccs/ont")
+
+    if args.aligner is None:
+        args.aligner = DATA_TYPES[args.data_type]
+        print(args.aligner + "will be used, make sure index is provided accordingly")
+    elif args.aligner not in ALIGNERS:
+        print("ERROR: specify aligner among assembly/10x_assembly/isoseq/ccs/ont")
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    if args.data_type == "10x_assembly":
+        for inf in args.fasta_files:
+            contigs_name, short_id_contigs_name = convert_fasta_with_barcodes(inf, args)
+            align_fasta(short_id_contigs_name, args, contigs_name)
+    else:
+        for inf in args.fasta_files:
+            align_fasta(inf, args)
+
+if __name__ == "__main__":
+   # stuff only to run when not called via 'import' here
+    try:
+        main()
+    except SystemExit:
+        raise
+    except:
+        print_exc()
+        sys.exit(-1)
+
 
 
