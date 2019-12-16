3749f0a1e23c80eebb543c7d8d160e5d92cd26c2
Andrey Prjibelski <andrewprzh@gmail.com>
2019-12-16 23:26:51 +0300

    all vs all tables

--- spiso-seq/exon-structure/exon_counts_to_tables.py
+++ spiso-seq/exon-structure/exon_counts_to_tables.py
@@ -19,7 +19,7 @@ def read_exon_counts(exon_count_file_name):
         if tokens[0] not in exon_counts_map:
             exon_counts_map[tokens[0]] = {}
         if tokens[1] in exon_counts_map[tokens[0]]:
-            print("Duplicating exon")
+            print("Duplicating exon " + tokens[0] + " " + tokens[1] + ": " + tokens[2] + ", " + tokens[3])
         exon_counts_map[tokens[0]][tokens[1]] = (int(tokens[2]), int(tokens[3]))
 
     return exon_counts_map
@@ -29,17 +29,17 @@ def process_exon(single_exon_counts):
     exon_count_tables = {}
     for cell_group in single_exon_counts.keys():
         cell_info = cell_group.split("_")
-        cell_type = cell_info[1]
+#        cell_type = cell_info[1]
 
-        cell_type_is_known = False
-        for ct in common_cell_types:
-            if cell_type.startswith(ct):
-                cell_type = ct
-                cell_type_is_known = True
-                break
-        if not cell_type_is_known:
-            continue
-
+#        cell_type_is_known = False
+#        for ct in common_cell_types:
+#            if cell_type.startswith(ct):
+#                cell_type = ct
+#                cell_type_is_known = True
+#                break
+#        if not cell_type_is_known:
+#            continue
+        cell_type = "all"
         if cell_type not in exon_count_tables:
             exon_count_tables[cell_type] = {"P7Hipp" : [0, 0], "P7PFC" : [0, 0]}
         tissue = cell_info[0]
@@ -61,6 +61,8 @@ def write_exon_count_table(exon_id, exon_count_tables, outf):
 def process_all_counts(exon_counts_map, out_file_name):
     outf = open(out_file_name, "w")
     for exon_id in sorted(exon_counts_map.keys()):
+        if exon_id.find("MULT") != -1:
+            continue
         exon_count_tables = process_exon(exon_counts_map[exon_id])
         write_exon_count_table(exon_id, exon_count_tables, outf)
     outf.close()
