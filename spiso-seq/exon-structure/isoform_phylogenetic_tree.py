81a19e2597cf2fa50281ecb6b4bc90dc0d058310
Andrey Prjibelski <andrewprzh@gmail.com>
2019-12-21 16:10:48 +0300

    phylo tree

--- spiso-seq/exon-structure/isoform_phylogenetic_tree.py
+++ spiso-seq/exon-structure/isoform_phylogenetic_tree.py
@@ -20,10 +20,12 @@ import networkx
 import matplotlib.pyplot as plt
 
 
-MIN_NODE_SIZE = 20
-MAX_NODE_SIZE = 1000
-EDGE_LEN_SCALE = 20
+MIN_NODE_SIZE = 40
+MAX_NODE_SIZE = 600
+EDGE_LEN_SCALE = 10
 DEFAULT_NODE_SIZE = 200
+
+CELL_GROUPS = ['P7Hipp_ExcitNeuron', 'P7Hipp_InhibNeuron', 'P7Hipp_GranuleNB', 'P7PFC_ExcitNeuron', 'P7PFC_InhibNeuron']
 
 def hamming_rel_dist(l1, l2):
     if len(l1) != len(l2):
@@ -33,7 +35,8 @@ def hamming_rel_dist(l1, l2):
 
 class IsoformPhylogeneticTreeConstructor:
 
-    def __init__(self, db, gene_list):
+    # cell_groups_isoform_counts: cell_group -> {isoform -> counts}
+    def __init__(self, db, gene_list, cell_groups_isoform_counts = {}):
         self.db = db
         self.chr_id = ""
 
@@ -47,43 +50,59 @@ class IsoformPhylogeneticTreeConstructor:
                 continue
 
             gene_db_list.append(gene_db)
-
         self.gene_info = GeneInfo(gene_db_list, db)
 
-    def make_tree(self, isoform_counts = {}):
-        exon_profiles = self.gene_info.all_rna_profiles.exon_profiles
-        self.isoform_counts = isoform_counts
-        if len(self.isoform_counts) == 0:
-            self.isoform_list = sorted(exon_profiles.keys())
+        self.exon_profiles = self.gene_info.all_rna_profiles.exon_profiles
+        self.cell_groups_isoform_counts = cell_groups_isoform_counts
+        if len(self.cell_groups_isoform_counts) == 0:
+            self.all_isoform_list = sorted(exon_profiles.keys())
         else:
-            self.isoform_list = sorted(self.isoform_counts.keys())
+            self.all_isoform_list = set()
+            self.max_count = 0
+            self.min_count = 100000000
+            isoform_totals = {}
 
+            self.max_isoform = ""
+            for cg in self.cell_groups_isoform_counts.keys():
+                self.max_count = max(self.max_count, max(self.cell_groups_isoform_counts[cg].values()))
+                self.min_count = min(self.min_count, min(self.cell_groups_isoform_counts[cg].values()))
+                for isoform in self.cell_groups_isoform_counts[cg].keys():
+                    self.all_isoform_list.add(isoform)
+                    if isoform not in isoform_totals:
+                        isoform_totals[isoform] = 0
+                    isoform_totals[isoform] += self.cell_groups_isoform_counts[cg][isoform]
+            self.all_isoform_list = sorted(list(self.all_isoform_list))
+            self.max_isoform = max(isoform_totals.iteritems(), key=operator.itemgetter(1))[0]
+
+            count_div = self.max_count - self.min_count
+            if count_div == 0:
+                self.size_scale = MAX_NODE_SIZE / (2 * self.max_count)
+            else:
+                self.size_scale = (MAX_NODE_SIZE - MIN_NODE_SIZE) / count_div
+
+    def make_tree(self, isoform_list):
         dm = []
-        for i1 in range(len(self.isoform_list)):
+        for i1 in range(len(isoform_list)):
             matrix_row = []
             for i2 in range(i1):
-                isoform1 = self.isoform_list[i1]
-                isoform2 = self.isoform_list[i2]
-                matrix_row.append(hamming_rel_dist(exon_profiles[isoform1][1:-1], exon_profiles[isoform2][1:-1]))
+                isoform1 = isoform_list[i1]
+                isoform2 = isoform_list[i2]
+                matrix_row.append(hamming_rel_dist(self.exon_profiles[isoform1][1:-1], self.exon_profiles[isoform2][1:-1]))
             matrix_row.append(0.0)
             dm.append(matrix_row)
 
-        self.distance_matrix = DistanceMatrix(self.isoform_list, dm)
+        distance_matrix = DistanceMatrix(isoform_list, dm)
         constructor = DistanceTreeConstructor()
-        self.tree = constructor.upgma(self.distance_matrix)
+        return constructor.upgma(distance_matrix)
 
-    def draw_tree(self):
-        max_isoform = ""
-        if len(self.isoform_counts) > 0:
-            max_isoform = max(self.isoform_counts.iteritems(), key=operator.itemgetter(1))[0]
-        if max_isoform != "":
-            self.tree.root_with_outgroup(max_isoform)
-        self.tree.clade.name = "InnerX"
-        net = Phylo.to_networkx(self.tree)
+    def draw_tree(self, tree, isoform_counts = {}, xshift = 0, yshift = 0, cell_group_name = ''):
+        tree.root_with_outgroup(self.max_isoform)
+        tree.clade.name = "InnerX"
+        net = Phylo.to_networkx(tree)
 
         nice_tree = networkx.DiGraph()
         for node in net.nodes:
-            nice_tree.add_node(node.name, size='100')
+            nice_tree.add_node(node.name)
         for e in net.edges:
             edge_len = EDGE_LEN_SCALE * e[1].branch_length
             if edge_len == 0:
@@ -100,29 +119,71 @@ class IsoformPhylogeneticTreeConstructor:
                 node_labels[node] = ' '
             else:
                 node_labels[node] = node
-                if len(self.isoform_counts) > 0:
-                    count_div = max(self.isoform_counts.values()) - min(self.isoform_counts.values())
-                    if count_div == 0:
-                        size_scale = MAX_NODE_SIZE / (2 * max(self.isoform_counts.values()))
+                if len(isoform_counts) > 0:
+                    if node not in isoform_counts:
+                        size = MIN_NODE_SIZE
                     else:
-                        size_scale = (MAX_NODE_SIZE - MIN_NODE_SIZE) / count_div
-                    size = MIN_NODE_SIZE + (self.isoform_counts[node] - min(self.isoform_counts.values())) * size_scale
+                        size = MIN_NODE_SIZE + (isoform_counts[node] - self.min_count) * self.size_scale
                     node_sizes.append(size)
-                    node_colors.append('red' if node == max_isoform else 'green')
+                    node_colors.append('red' if node == self.max_isoform else 'green')
                 else:
                     node_sizes.append(DEFAULT_NODE_SIZE)
                     node_colors.append('green')
                     node_labels[node] = node
 
+
         pos = networkx.drawing.nx_pydot.pydot_layout(nice_tree)
-        networkx.drawing.nx_pydot.write_dot(nice_tree, "1.dot")
-        networkx.draw(nice_tree, pos, with_labels = True, labels=node_labels, node_color=node_colors, node_size=node_sizes)
-        plt.show()
+        new_pos = {}
+        for k in pos:
+            new_pos[k] = (pos[k][0] + xshift, pos[k][1] + yshift)
+        # TODO: manually draw labels
+        networkx.draw(nice_tree, new_pos, with_labels=True, labels=node_labels, node_color=node_colors,
+                      node_size=node_sizes, font_size = '4')
+#        plt.show()
+
+    def draw_trees(self, outf_name):
+        hipp_count = 0
+        pfc_count = 0
+        for cg in CELL_GROUPS:
+            yshift = 0
+            xshift = 0
+            if cg.startswith("P7Hipp"):
+                xshift = 600 * hipp_count
+                hipp_count += 1
+            else:
+                yshift = 400
+                xshift = 600 * pfc_count
+                pfc_count += 1
+
+            t = self.make_tree(self.all_isoform_list)
+            self.draw_tree(t, self.cell_groups_isoform_counts[cg], xshift, yshift)
+        plt.savefig(outf_name, format='svg')
+
+
+def read_counts(inf):
+    isoform_counts = {}
+    for l in open(inf):
+        tokens = l.strip().split()
+        if len(tokens) != 3:
+            continue
+        isoform = tokens[0]
+        cg = tokens[1]
+        for c in CELL_GROUPS:
+            if cg.startswith(c):
+                cg = c
+        count = int(tokens[2])
+        if cg not in isoform_counts:
+            isoform_counts[cg] = {}
+        if isoform not in isoform_counts[cg]:
+            isoform_counts[cg][isoform] = 0
+        isoform_counts[cg][isoform] += count
+    return isoform_counts
 
 
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('gene_ids', metavar='BAM_FILE', nargs='+', type=str,  help='gene ids for tree construction')
+    parser.add_argument('--gene_id', type=str,  help='gene ids for tree construction')
+    parser.add_argument('--counts', type=str, help='counts')
     parser.add_argument("--genedb", "-g", help="gene database in gffutils db format", type=str)
     args = parser.parse_args()
     return args
@@ -135,10 +196,13 @@ def main():
         raise Exception("Gene database " + args.genedb + " does not exist")
     db = gffutils.FeatureDB(args.genedb, keep_order=True)
 
-    isoform_tree_constructor = IsoformPhylogeneticTreeConstructor(db, args.gene_ids)
-    isoform_counts = {'ENSMUST00000217664.1' : 61, 'ENSMUST00000217810.1' : 215, 'ENSMUST00000218469.1' : 29, 'ENSMUST00000218480.1' : 81}
-    isoform_tree_constructor.make_tree(isoform_counts)
-    isoform_tree_constructor.draw_tree()
+    isoform_counts = read_counts(args.counts)
+    isoform_tree_constructor = IsoformPhylogeneticTreeConstructor(db, [args.gene_id], isoform_counts)
+    isoform_tree_constructor.draw_trees(args.gene_id + ".svg")
+
+#    isoform_counts = {'ENSMUST00000217664.1' : 61, 'ENSMUST00000217810.1' : 215, 'ENSMUST00000218469.1' : 29, 'ENSMUST00000218480.1' : 81}
+#    isoform_tree_constructor.make_tree(isoform_counts)
+#    isoform_tree_constructor.draw_tree()
 
 
 if __name__ == "__main__":
