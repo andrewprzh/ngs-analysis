af59931e70cbf2804c1839fe6e40dc3f03be1e9a
Andrey Prjibelski <andrewprzh@gmail.com>
2020-04-02 14:45:10 +0300

    compare mismapped reads

--- spiso-seq/compare_long_reads/map_misaligned_reads.py
+++ spiso-seq/compare_long_reads/map_misaligned_reads.py
@@ -10,27 +10,32 @@ import sys
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
-from Bio.Alphabet import IUPAC
 
 
 inf = open(sys.argv[1])
-l = "1"
+l = inf.readline()
 
 while l:
-    reads = inf.readline().strip().replace('/', '-')
+    reads = l.strip().replace('/', '-')
     pos = inf.readline().strip()
 
     seq1 = inf.readline().strip()
     seq2 = inf.readline().strip()
-    seq1_file = reads.split()[0] + ".fasta"
-    seq2_file = reads.split()[1] + ".fasta"
+    read_id1 = reads.split()[0]
+    read_id2 = reads.split()[1]
+    seq1_file = read_id1 + ".fasta"
+    seq2_file = read_id2 + ".fasta"
 
-    record = SeqRecord(Seq(seq1, IUPACAmbiguousDNA()), name=reads.split()[0])
+    record = SeqRecord(Seq(seq1), id=read_id1, description=pos.split()[0])
     SeqIO.write([record], seq1_file, "fasta")
-    record = SeqRecord(Seq(seq2, IUPACAmbiguousDNA()), name=reads.split()[1])
+    record = SeqRecord(Seq(seq2), id=read_id2, description=pos.split()[1])
     SeqIO.write([record], seq2_file, "fasta")
 
+    sam_out = "sam/" + read_id1 +  "_" + read_id2 + ".sam"
     os.system("/Bmo/prjbel/tools/minimap2-2.8_x64-linux/minimap2 "
-              "-t 1 -x ava-ont " + seq2_file + " " + seq1_file + " > " + seq1_file + "_" + seq2_file + ".sam")
-    break
+              "-t 1 -x ava-ont " + seq2_file + " " + seq1_file + " > " + sam_out)
+
+    if os.stat(sam_out).st_size == 0:
+        os.remove(sam_out)
+    l = inf.readline()
 
