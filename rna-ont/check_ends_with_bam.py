ec30dd5c34a4eaadd0766904b5e2a72a2302c4b2
Andrey Prjibelski <andrewprzh@gmail.com>
2020-04-24 12:39:20 +0300

    checking start/end coordinates frequency in bam

---
+++ rna-ont/check_ends_with_bam.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import pysam
+import numpy
+
+def find_starts(bam_filename):
+    bam = pysam.AlignmentFile(bam_filename, "rb")
+    starts = []
+    for alignment in bam.fetch():
+        if alignment.reference_id == -1 or alignment.is_secondary:
+            continue
+        blocks = sorted(alignment.get_blocks())
+        start = blocks[0][0]
+        starting_cigar = alignment.cigartuples[0]
+        if starting_cigar[0] == 4:
+            start -= starting_cigar[1]
+        starts.append(start)
+    return starts
+
+
+def find_ends(bam_filename):
+    bam = pysam.AlignmentFile(bam_filename, "rb")
+    ends = []
+    for alignment in bam.fetch():
+        if alignment.reference_id == -1 or alignment.is_secondary:
+            continue
+        blocks = sorted(alignment.get_blocks())
+        end = blocks[-1][1]
+        ending_cigar = alignment.cigartuples[0]
+        if ending_cigar[0] == 4:
+            end += ending_cigar[1]
+            ends.append(end)
+    return ends
+
+
+def print_hist(coords):
+    hist = {}
+    for v in coords:
+        if v not in hist:
+            hist[v] = 0
+        hist[v] += 1
+    for k in sorted(hist.keys()):
+        print(str(k) + "\t" + str(hist[k]))
+
+
+if sys.argv[2].startswith("e"):
+    coords = find_ends(sys.argv[1])
+else:
+    coords = find_starts(sys.argv[1])
+
+print_hist(coords)
