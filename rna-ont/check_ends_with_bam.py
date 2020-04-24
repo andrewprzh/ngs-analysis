30839e88585ad997558ad0f74c2f00b162ae44d9
Andrey Prjibelski <andrewprzh@gmail.com>
2020-04-24 17:37:23 +0300

    fixed rna-ont/check_ends_with_bam.py

--- rna-ont/check_ends_with_bam.py
+++ rna-ont/check_ends_with_bam.py
@@ -10,6 +10,9 @@ def find_starts(bam_filename):
         if alignment.reference_id == -1 or alignment.is_secondary:
             continue
         blocks = sorted(alignment.get_blocks())
+        if len(blocks) == 0:
+            continue
+
         start = blocks[0][0]
         starting_cigar = alignment.cigartuples[0]
         if starting_cigar[0] == 4:
@@ -25,22 +28,31 @@ def find_ends(bam_filename):
         if alignment.reference_id == -1 or alignment.is_secondary:
             continue
         blocks = sorted(alignment.get_blocks())
+        if len(blocks) == 0:
+            continue
+
         end = blocks[-1][1]
-        ending_cigar = alignment.cigartuples[0]
+        ending_cigar = alignment.cigartuples[-1]
         if ending_cigar[0] == 4:
             end += ending_cigar[1]
             ends.append(end)
     return ends
 
 
-def print_hist(coords):
+def print_hist(coords, flag):
     hist = {}
     for v in coords:
         if v not in hist:
             hist[v] = 0
         hist[v] += 1
-    for k in sorted(hist.keys()):
-        print(str(k) + "\t" + str(hist[k]))
+    positions = sorted(hist.keys())
+    if not flag.startswith("e"):
+        interval = range(max(-50, positions[0]), min(101, positions[-1] + 1))
+    else:
+        interval = range(max(37000, positions[0]), min(37401, positions[-1] + 1))
+    for k in interval:
+        v = 0 if k not in hist else hist[k]
+        print(str(k) + "\t" + str(v))
 
 
 if sys.argv[2].startswith("e"):
@@ -48,4 +60,4 @@ if sys.argv[2].startswith("e"):
 else:
     coords = find_starts(sys.argv[1])
 
-print_hist(coords)
+print_hist(coords,  sys.argv[2])
