6ea94ec168b5e704ff9904c18cba378d86c73358
Andrey Prjibelski <andrewprzh@gmail.com>
2020-04-30 18:25:04 +0300

    proper minimap params

--- spiso-seq/preprocessing/common.py
+++ spiso-seq/preprocessing/common.py
@@ -94,7 +94,7 @@ def align_fasta(contigs_infile, args, out_name = ""):
 
     elif method.lower() == "minimap":
         minimap_path = '/Bmo/prjbel/tools/minimap2-2.8_x64-linux/minimap2'
-        command =  minimap_path+ ' {ref_index_name} {transcripts} -a -x splice -t {threads} ' \
+        command =  minimap_path+ ' {ref_index_name} {transcripts} -a -x splice --secondary=no -C5  -t {threads} ' \
                               '> {alignment_out} '.format(ref_index_name=args.index,
                                                           transcripts=contigs_infile,
                                                           threads=str(args.threads),
