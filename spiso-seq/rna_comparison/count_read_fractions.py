b77b280e1cd47a2131594aca3bbb1154b854a40c
Andrey Prjibelski <andrewprzh@gmail.com>
2021-04-12 16:40:13 +0300

    3 introns

--- spiso-seq/rna_comparison/count_read_fractions.py
+++ spiso-seq/rna_comparison/count_read_fractions.py
@@ -28,12 +28,20 @@ for l in open(sys.argv[2]):
 
 print("Calculating histograms")
 for intron_count in sorted(read_fraction_arrays.keys()):
-    if intron_count <= 10:
+    if intron_count <= 3:
         continue
-    read_fraction_arrays[10] += read_fraction_arrays[intron_count]
+    read_fraction_arrays[3] += read_fraction_arrays[intron_count]
 
 bins = [0.1 * i for i in range(11)]
-for intron_count in range(11):
+print("Histogram for bin 0.1")
+for intron_count in range(1,4):
     hist = numpy.histogram(read_fraction_arrays[intron_count], bins, density=True)[0]
-    print("%d\t%s" % (intron_count, "\t".join([".4f" % x for x in hist])))
+    print("%d\t%s" % (intron_count, "\t".join(["%.4f" % (x * 10) for x in hist])))
+
+bins = [0.2 * i for i in range(6)]
+print("Histogram for bin 0.2")
+for intron_count in range(1,4):
+    hist = numpy.histogram(read_fraction_arrays[intron_count], bins, density=True)[0]
+    print("%d\t%s" % (intron_count, "\t".join(["%.4f" % (x * 20) for x in hist])))
+
 
