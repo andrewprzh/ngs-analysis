3389720c80436aaa97fe4b8ecc7424cb6928c95d
Andrey Prjibelski <andrewprzh@gmail.com>
2021-04-14 14:58:32 +0300

    check supported introns

--- spiso-seq/rna_comparison/inconsistency_stats.py
+++ spiso-seq/rna_comparison/inconsistency_stats.py
@@ -2,32 +2,74 @@ import numpy
 import sys
 from collections import defaultdict
 
-match_types = sorted(["extra_intron_novel", "fake_terminal_exon_3", "fake_terminal_exon_5", "extra_intron_5", "extra_intron_3"])
 
-read_info_map = {}
+def print_stats(read_intron_dict, max_len = 5):
+    # sum up everythin above max_len
+    for intron_count in sorted(read_intron_dict.keys()):
+        if intron_count <= max_len:
+            continue
+        for k in read_intron_dict[intron_count].keys():
+            read_intron_dict[max_len][k] += read_intron_dict[intron_count][k]
+            read_intron_dict[intron_count][k] = 0
+
+    print("introns\t" + "\t".join(match_types))
+    for intron_count in sorted(read_intron_dict.keys()):
+        if intron_count > max_len:
+            continue
+        print("%d\t%s" % (
+        intron_count, "\t".join([str(read_intron_dict[intron_count][x]) for x in ["total_reads"] + match_types])))
+
+    print("introns\t" + "\t".join(match_types))
+    for intron_count in sorted(read_intron_dict.keys()):
+        if intron_count > max_len:
+            continue
+        total_reads = read_intron_dict[intron_count]["total_reads"]
+        fractions = [(100 * read_intron_dict[intron_count][x] / total_reads) for x in match_types]
+        print("%d\t%s" % (intron_count, "\t".join(["%.2f" % x for x in fractions])))
+
+
+match_types = (["extra_intron_novel", "fake_terminal_exon_3", "fake_terminal_exon_5", "extra_intron_5", "extra_intron_3", "alternative_structure_novel"])
+
+
+supported_introns = set()
+for l in open(sys.argv[2]):
+    if l.strip("EN"):
+        continue
+    t = l.strip().split("-")
+    supported_introns.add((int(t[0]), int(t[1])))
+
+read_intron_dict = {}
+confirmed_reads_introns = {}
 for l in open(sys.argv[1]):
     t = l.strip().split()
     if t[2] != "inconsistent":
         continue
     read_id = t[0]
     intron_count = t[4].count(",")
-    if intron_count not in read_info_map:
-        read_info_map[intron_count] = defaultdict(int)
+    if intron_count not in read_intron_dict:
+        read_intron_dict[intron_count] = defaultdict(int)
 
     for mt in match_types:
-        if t[3].find(mt) != -1:
-            read_info_map[intron_count][mt] += 1
-    read_info_map[intron_count]["total_reads"] += 1
+        event_info = t[3]
+        event_pos = event_info.find(mt)
+        if event_pos != -1:
+            read_intron_dict[intron_count][mt] += 1
+            next_event = event_info.find(",", event_pos)
+            if next_event == -1:
+                intron_str = event_info[event_pos+len(mt)+1:]
+            else:
+                intron_str = event_info[event_pos + len(mt) + 1:next_event]
+            t = intron_str.split("-")
+            intron = (int(t[0]), int(t[1]))
+            if intron in supported_introns:
+                confirmed_reads_introns[intron_count][mt] += 1
 
-print("introns\t" + "\t".join(match_types))
-for intron_count in sorted(read_info_map.keys()):
-    print("%d\t%s" % (intron_count, "\t".join([str(read_info_map[intron_count][x]) for x in ["total_reads"] + match_types])))
+    read_intron_dict[intron_count]["total_reads"] += 1
+    confirmed_reads_introns[intron_count]["total_reads"] += 1
 
-print("introns\t" + "\t".join(match_types))
-for intron_count in sorted(read_info_map.keys()):
-    total_reads = read_info_map[intron_count]["total_reads"]
-    fractions = [(read_info_map[intron_count][x] / total_reads) for x in match_types]
-    print("%d\t%s" % (intron_count, "\t".join(["%.4f" % x for x in fractions])))
+print_stats(read_intron_dict)
+print_stats(confirmed_reads_introns)
+
 
 
 
