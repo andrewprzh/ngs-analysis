d3c93e31d22abdb64ca06d934de37516aa893a5c
Andrey Prjibelski <andrewprzh@gmail.com>
2021-05-13 12:44:56 +0300

    fixes for mapping

--- spiso-seq/exon-structure/align_exons.py
+++ spiso-seq/exon-structure/align_exons.py
@@ -4,6 +4,7 @@ import argparse
 from Bio import SeqIO
 import gzip
 from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
 from collections import defaultdict
 import numpy
 from traceback import print_exc
@@ -24,10 +25,11 @@ class ExonPair:
 def load_exon_pairs(inf):
     print("Loading exon pairs")
     exon_storage = defaultdict(list)
+    total = 0
     for l in open(inf):
         t = l.strip().split()
         exon1_t = t[0].split("_")
-        exon2_t = t[0].split("_")
+        exon2_t = t[1].split("_")
         assert exon2_t[0] == exon1_t[0]
         assert exon2_t[3] == exon1_t[3]
         chr_id = exon1_t[0]
@@ -35,6 +37,8 @@ def load_exon_pairs(inf):
         exon2 = (int(exon2_t[1]), int(exon2_t[2]))
         exon_pair = ExonPair(chr_id, exon1_t[3], exon1, exon2, t[3], t[2])
         exon_storage[chr_id].append(exon_pair)
+        total += 1
+    print("Loaded %d pairs" % total)
     return exon_storage
 
 
@@ -54,11 +58,13 @@ class ExonComparator:
         for chr_id, exon_list in exon_storage.items():
             chr_record = self.reference_record_dict[chr_id]
             for exon_pair in exon_list:
-                exon1_seq = chr_record[exon_pair.exon1[0]:exon_pair.exon1[1] + 1]
-                exon2_seq = chr_record[exon_pair.exon2[0]:exon_pair.exon2[1] + 1]
+                exon1_seq = str(chr_record[exon_pair.exon1[0]:exon_pair.exon1[1] + 1].seq)
+                exon2_seq = str(chr_record[exon_pair.exon2[0]:exon_pair.exon2[1] + 1].seq)
                 exon_min_len = min(len(exon1_seq), len(exon2_seq))
-                exon_pair.global_similarity = pairwise2.align.globalxx(exon1_seq, exon2_seq, score_only = True) / exon_min_len
-                exon_pair.local_similarity = pairwise2.align.localxx(exon1_seq, exon2_seq, score_only=True) / exon_min_len
+                exon_pair.global_similarity = pairwise2.align.globalms(exon1_seq, exon2_seq, 1, -1, -1, -1, score_only = True) / exon_min_len
+                local_al = pairwise2.align.localms(exon1_seq, exon2_seq, 1, -1, -1, -1, score_only=False)[0]
+                print(format_alignment(*local_al))
+                exon_pair.local_similarity = pairwise2.align.localms(exon1_seq, exon2_seq, 1, -1, -1, -1, score_only=True) / exon_min_len
 
     def compute_hist(self, exon_storage, property_func=lambda x:x.global_similarity):
         print("Computing histograms")
@@ -66,11 +72,12 @@ class ExonComparator:
         for chr_id, exon_list in exon_storage.items():
             for exon_pair in exon_list:
                 exon_similarity_map[exon_pair.exon_type].append(property_func(exon_pair))
-
+        #print(exon_similarity_map)
         hist_map = {}
         bins = [0.1 * i for i in range(11)]
         for exon_type, similarity_values in exon_similarity_map.items():
             hist_map[exon_type] = numpy.histogram(similarity_values, bins)[0]
+        #print(hist_map)
         return hist_map
 
 
@@ -93,13 +100,13 @@ def main():
     exon_comparator.map_exon_pairs(exon_storage)
     outf = open(args.output, "w")
     hist_map = exon_comparator.compute_hist(exon_storage, property_func=lambda x:x.global_similarity)
-    outf.write("Global similarity histograms")
-    for exon_type, hist in hist_map:
-        outf.write(exon_type + "\t" + "\t".join([str(x) for x in hist]))
+    outf.write("Global similarity histograms\n")
+    for exon_type, hist in hist_map.items():
+        outf.write(exon_type + "\t" + "\t".join([str(x) for x in hist]) + "\n")
     hist_map = exon_comparator.compute_hist(exon_storage, property_func=lambda x: x.local_similarity)
-    outf.write("Local similarity histograms")
-    for exon_type, hist in hist_map:
-        outf.write(exon_type + "\t" + "\t".join([str(x) for x in hist]))
+    outf.write("Local similarity histograms\n")
+    for exon_type, hist in hist_map.items():
+        outf.write(exon_type + "\t" + "\t".join([str(x) for x in hist]) + "\n")
     print("Done")
 
 
