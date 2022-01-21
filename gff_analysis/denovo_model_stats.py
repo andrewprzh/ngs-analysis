5c12d87122da45d3d36c069bd8641ef59d22be43
Andrey Prjibelski <andrewprzh@gmail.com>
2022-01-21 14:41:17 +0300

    add one more category

--- gff_analysis/denovo_model_stats.py
+++ gff_analysis/denovo_model_stats.py
@@ -23,6 +23,7 @@ def parse_args():
 
 def process_tracking(gffcompare_output_prefix, gtf_num):
     reliable_transcripts = 0
+    almost_reliable = [0] * gtf_num # supported by gtf_num - 2 tools
     unique_transcripts = [0] * gtf_num
     missed_transcripts = [0] * gtf_num
     total_transcripts = [0] * gtf_num
@@ -40,6 +41,10 @@ def process_tracking(gffcompare_output_prefix, gtf_num):
 
         if matches_transcripts == gtf_num:
             reliable_transcripts += 1
+        elif matches_transcripts == gtf_num - 1:
+            for i, e in enumerate(equality_vector):
+                if e == 1:
+                    almost_reliable[i] += 1
         elif matches_transcripts == 1:
             index = equality_vector.index(1)
             unique_transcripts[index] += 1
@@ -47,14 +52,16 @@ def process_tracking(gffcompare_output_prefix, gtf_num):
             index = equality_vector.index(0)
             missed_transcripts[index] += 1
 
-    return reliable_transcripts, total_transcripts, unique_transcripts, missed_transcripts
+    return reliable_transcripts, almost_reliable, total_transcripts, unique_transcripts, missed_transcripts
 
 
-def print_stats(name, reliable_transcripts, total_transcripts, unique_transcripts, missed_transcripts, out=sys.stdout):
+def print_stats(name, reliable_transcripts, almost_reliable, total_transcripts, unique_transcripts, missed_transcripts, out=sys.stdout):
     out.write(name + "\n")
     total_columns = len(total_transcripts)
     out.write("\t".join([str(reliable_transcripts)] * total_columns) + "\n")
-    out.write("\t".join([str(total_transcripts[i] - unique_transcripts[i] - reliable_transcripts) for i in range(total_columns)]) + "\n")
+    out.write("\t".join([str(almost_reliable[i]) for i in range(total_columns)]) + "\n")
+    out.write("\t".join([str(total_transcripts[i] - unique_transcripts[i] - almost_reliable[i] - reliable_transcripts)
+                         for i in range(total_columns)]) + "\n")
     out.write("\t".join([str(unique_transcripts[i]) for i in range(total_columns)]) + "\n")
     out.write("\t".join([str(-missed_transcripts[i]) for i in range(total_columns)]) + "\n")
     out.flush()
@@ -104,9 +111,9 @@ def main():
         if not os.path.exists(gff_compare_out + ".tracking"):
             print("Running gffcompare for " + name)
             run_gff_compare_noref(gtf_list, gff_compare_out)
-        reliable_transcripts, total_transcripts, unique_transcripts, missed_transcripts = \
+        reliable_transcripts, almost_reliable, total_transcripts, unique_transcripts, missed_transcripts = \
             process_tracking(gff_compare_out, len(gtf_list))
-        print_stats(name, reliable_transcripts, total_transcripts, unique_transcripts, missed_transcripts, out=out_stats)
+        print_stats(name, reliable_transcripts, almost_reliable, total_transcripts, unique_transcripts, missed_transcripts, out=out_stats)
     out_stats.close()
 
 
