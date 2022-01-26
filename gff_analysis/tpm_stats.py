16531cdbd3ff5b42c9064928edb252282d5fa285
Andrey Prjibelski <andrewprzh@gmail.com>
2022-01-26 03:53:56 +0300

    fixes in tpm

--- gff_analysis/tpm_stats.py
+++ gff_analysis/tpm_stats.py
@@ -35,6 +35,7 @@ def load_counts(counts_file):
         if len(t) < 3:
             continue
         count_dict[t[0]] = float(t[2])
+    print("Loaded %d counts" % len(count_dict))
     return count_dict
 
 
@@ -51,13 +52,15 @@ def load_transcripts(gtf_file):
 
         for i, v in enumerate(t):
             if v == 'transcript_id':
-                transcripts.add(t[i+1])
+                transcripts.add(t[i+1][1:-2])
                 break
+    print("Loaded %d reference transcripts" % len(transcripts))
     return transcripts
 
 
 def count_stats(tracking_file, count_dict, transcript_set, bins):
-    transcript_hist = defaultdict()
+    transcript_hist = defaultdict(int)
+    processed_transcripts = set()
     for l in open(tracking_file):
         t = l.strip().split()
         if t[2] == '-' or t[3] != '=':
@@ -66,41 +69,49 @@ def count_stats(tracking_file, count_dict, transcript_set, bins):
         if transcript_id not in transcript_set:
             continue
         if transcript_id not in count_dict:
-            sys.stderr.write("Counts for transcript %s is not found" % transcript_id)
+            sys.stderr.write("Counts for transcript %s is not found\n" % transcript_id)
+            continue
+        if transcript_id in processed_transcripts:
             continue
 
         tpm = count_dict[transcript_id]
         for b in reversed(bins):
             if tpm >= b:
                 transcript_hist[b] += 1
+                processed_transcripts.add(transcript_id)
+                break
     return transcript_hist
 
 
 def count_total(count_dict, transcript_set, bins):
-    transcript_hist = defaultdict()
+    transcript_hist = defaultdict(int)
     for transcript_id in transcript_set:
         if transcript_id not in count_dict:
-            sys.stderr.write("Counts for transcript %s is not found" % transcript_id)
+            sys.stderr.write("Counts for reference transcript %s is not found\n" % transcript_id)
             continue
 
         tpm = count_dict[transcript_id]
         for b in reversed(bins):
             if tpm >= b:
                 transcript_hist[b] += 1
-        return transcript_hist
+                break
+    return transcript_hist
 
 
 def main():
     args = parse_args()
-    bins = [0, 5, 10, 50, 100, 500]
+    bins = [0, 5, 10, 50, 100]
     count_dict = load_counts(args.counts)
     transcript_set = load_transcripts(args.genedb)
     total_hist = count_total(count_dict, transcript_set, bins)
     restored_hist = count_stats(args.tracking, count_dict, transcript_set, bins)
 
+    print(args.tracking)
     print("\t".join(map(str, bins)))
-    print("\t".join(map(str, [restored_hist[b] for b in bins])))
-    print("\t".join(map(str, [total_hist[b] for b in bins])))
+    print("\t".join(map(str, [restored_hist[b] for b in bins])) + "\t%d" % sum(restored_hist.values()))
+    print("\t".join(map(str, [total_hist[b] for b in bins])) + "\t%d" % sum(total_hist.values()))
+    print("\t".join(["%.2f" % (restored_hist[b] / total_hist[b]) for b in bins]) + "\t%.2f" % (sum(restored_hist.values()) / sum(total_hist.values())))
+    print(" ")
 
 
 if __name__ == "__main__":
