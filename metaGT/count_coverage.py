8eea7700a5a9f20464a25f32c731b0913a4c15c1
Andrey Prjibelski <andrewprzh@gmail.com>
2022-08-17 18:01:29 +0300

    count misassemblies

--- metaGT/count_coverage.py
+++ metaGT/count_coverage.py
@@ -1,29 +1,124 @@
+#!/usr/bin/env python3
+#
+############################################################################
+# Copyright (c) 2020 Saint Petersburg State University
+# # All Rights Reserved
+# See file LICENSE for details.
+############################################################################
 import sys
 import pysam
+import argparse
+from traceback import print_exc
+from collections import defaultdict
 
-cov90 = set()
-match90 = set()
-unaligned = set()
-all_contigs = set()
 
-samf = pysam.AlignmentFile(sys.argv[1], "r")
-for a in samf:
-    all_contigs.add(a.query_name)
-    if a.is_unmapped:
-        unaligned.add(a.query_name)
-        continue
+COVERAGE_PERCENT = 0.95
+MAX_OVERLAP = 0.2
 
-    ctg_len = a.query_length
-    ctg_aligned = a.query_alignment_length
-    ref_len = samf.get_reference_length(a.reference_name)
-    ref_aligned = a.reference_length
 
-    if ref_aligned >=  ref_len * 0.975:
-        cov90.add(a.reference_name)
-        if ctg_aligned >= ctg_len * 0.975:
-            match90.add(a.reference_name)
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("--bam", "-b", type=str, help="BAM file to assess")
+    args = parser.parse_args()
+    if not args.isoquant_prefix or not args.gffcompare_tracking or not args.genedb or not args.isoquantdb:
+        parser.print_usage()
+        exit(-1)
+    return args
 
-print("Total: ", len(all_contigs))
-print("Unmapped: ", len(unaligned))
-print("Genes covered: ", len(cov90))
-print("Genes covered no extra", len(match90))
+
+def count_coverage(samf):
+    cov90 = set()
+    match90 = set()
+    unaligned = set()
+    all_contigs = set()
+
+    for a in samf:
+        all_contigs.add(a.query_name)
+        if a.is_unmapped:
+            unaligned.add(a.query_name)
+            continue
+
+        ctg_len = a.query_length
+        ctg_aligned = a.query_alignment_length
+        ref_len = samf.get_reference_length(a.reference_name)
+        ref_aligned = a.reference_length
+
+        if ref_aligned >=  ref_len * COVERAGE_PERCENT:
+            cov90.add(a.reference_name)
+            if ctg_aligned >= ctg_len * COVERAGE_PERCENT:
+                match90.add(a.reference_name)
+
+    print("Total: ", len(all_contigs))
+    print("Unmapped: ", len(unaligned))
+    print("Genes covered: ", len(cov90))
+    print("Genes covered no extra", len(match90))
+
+
+def interval_len(interval):
+    return max(0, interval[1] - interval[0] + 1)
+
+
+def overlap_intervals(range1, range2):
+    return (max(range1[0], range2[0]), min(range1[1], range2[1]))
+
+
+def overlap_fraction(range1, range2):
+    return interval_len(overlap_intervals(range1, range2)) / min(interval_len(range1), interval_len(range2))
+
+
+def is_misassembled(alignment_list):
+    primary_alignment = None
+    for a in alignment_list:
+        if a.is_secondary or a.is_supplementary:
+            continue
+        primary_alignment = a
+        break
+
+    if not primary_alignment:
+        return False
+
+    for a in alignment_list:
+        if not a.is_supplementary:
+            continue
+        if primary_alignment.reference_name != a.reference_name and \
+                overlap_fraction((primary_alignment.query_alignment_start, primary_alignment.query_alignment_end),
+                                 (a.query_alignment_start, a.query_alignment_end)) <= MAX_OVERLAP:
+            return True
+
+    return False
+
+
+def count_misassemblies(samf):
+    alignment_dict = defaultdict(list)
+
+    for a in samf:
+        if a.is_unmapped:
+            continue
+        alignment_dict[a.query_name].append(a)
+
+    candidates = 0
+    misassemblies = 0
+    for contig_name in alignment_dict.keys():
+        if len(alignment_dict[contig_name]) == 1:
+            continue
+        candidates += 1
+        if is_misassembled(alignment_dict[contig_name]):
+            misassemblies += 1
+
+    print("Candidates: %d, misassemblies %d" % (candidates, misassemblies))
+
+def main():
+    args = parse_args()
+    count_coverage(pysam.AlignmentFile(args.bam, "r"))
+
+if __name__ == "__main__":
+   # stuff only to run when not called via 'import' here
+    try:
+        main()
+    except SystemExit:
+        raise
+    except:
+        print_exc()
+        sys.exit(-1)
+
+
