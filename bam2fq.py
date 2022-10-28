9e37e2cd0f1a6a4350836e23fcbdb21a2e700665
Andrey Prjibelski <andrewprzh@gmail.com>
2022-10-28 17:10:25 +0300

    simple bam2fq

---
+++ bam2fq.py
@@ -0,0 +1,29 @@
+import sys
+import pysam
+from Bio import SeqIO
+from Bio import SeqRecord
+from Bio import Seq
+
+open(sys.argv[2], "w").close()
+handle = open(sys.argv[2], "a")
+buffer = []
+processed_ids = set()
+
+for a in pysam.AlignmentFile(sys.argv[1], "r"):
+    seq = a.get_forward_sequence()
+    if not seq:
+        continue
+    read_id = a.query_name
+    if read_id in processed_ids:
+        continue
+    processed_ids.add(read_id)
+    qual = a.get_forward_qualities()
+
+    buffer.append(SeqRecord.SeqRecord(seq=Seq.Seq(seq), id=read_id, description="", letter_annotations={'phred_quality':qual}))
+
+    if len(buffer) > 100000:
+        SeqIO.write(buffer, handle, "fastq")
+        buffer = []
+
+SeqIO.write(buffer, handle, "fastq")
+handle.close()
