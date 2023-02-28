5f40832c0baf722db32b4066c1a863d138bcf920
Andrey Prjibelski <andrewprzh@gmail.com>
2023-03-01 01:18:04 +0200

    filter barcodes

--- ptbseq/preprocessing/filter_blaze_barcodes.py
+++ ptbseq/preprocessing/filter_blaze_barcodes.py
@@ -3,6 +3,11 @@ import sys
 
 inf = sys.argv[1]
 threshold = float(sys.argv[2])
+whitelist = sys.argv[3]
+
+whitelist_set = set()
+for l in open(whitelist):
+    whitelist_set.add(l.split()[0])
 
 for l in open(inf):
     if l.startswith("read_id,"):
@@ -11,7 +16,7 @@ for l in open(inf):
     if len(v) < 3 or not v[2]:
         continue
     q = float(v[2])
-    if q < threshold:
+    if v[1] not in whitelist_set or q < threshold:
         continue
 
     print("%s\t%s" % (v[0], v[1]))
