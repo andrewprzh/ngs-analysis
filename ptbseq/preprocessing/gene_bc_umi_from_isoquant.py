b655d20ae014d4c6b9fb143f0474800b3b626559
Andrey Prjibelski <andrewprzh@gmail.com>
2023-03-02 15:32:46 +0200

    generate triplets

---
+++ ptbseq/preprocessing/gene_bc_umi_from_isoquant.py
@@ -0,0 +1,20 @@
+import sys
+
+# usage: <read-barcode-umi> <read_assignment.tsv>
+
+read_bc_umi = {}
+for l in open(sys.argv[1]):
+    v = l.strip().split('\t')
+    read_bc_umi[v[0][1:]] = (v[1], v[2])
+
+bc_umi_gene = set()
+for l in open(sys.argv[2]):
+    v = l.strip().split('\t')
+    if v[3] == '.':
+        continue
+    read_id = v[0]
+    if read_id in read_bc_umi:
+        bc_umi_gene.add((v[3], read_bc_umi[read_id][0], read_bc_umi[read_id][1]))
+
+for t in bc_umi_gene:
+    print("%s\t%s\t%s" % t)
