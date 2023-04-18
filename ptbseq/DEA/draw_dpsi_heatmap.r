6613fa87d8a8dbb9d09959caf8eadc07c2278fb7
Andrey Prjibelski <andrewprzh@gmail.com>
2023-04-18 14:11:32 +0300

    na rows

--- ptbseq/DEA/draw_dpsi_heatmap.r
+++ ptbseq/DEA/draw_dpsi_heatmap.r
@@ -1,7 +1,8 @@
 library(gplots)
+library(dplyr)
 
 # Read normalized counts
-data = read.table("/home/andreyp/ablab/analysis/pertrubseq/dee/heatmaps/allVSnegative.dPSI_heatmap.tsv", header=T, sep="\t", as.is=TRUE)
+  data = read.table("/home/andreyp/ablab/analysis/pertrubseq/dee/SCv3.UMI_filtered.exon_filtered.21.03/full_guides/oneVScontrol.dPSI.defaults.tsv", header=T, sep="\t", as.is=TRUE)
 
 genes = data[,1]
 mat = as.matrix(data[,2:ncol(data)])
@@ -12,4 +13,21 @@ my_palette <- colorRampPalette(c("red", "white", "blue")) (n=20)
 breaks <- seq(min(mat, na.rm = T), max(mat, na.rm = T), length.out = 21)
 heatmap.2(mat, trace="none", na.color = "black", scale="none", 
           col = my_palette)
+
+
+
+subs_m <- as.matrix(data) ## Main matrix
+
+DR <- dist(subs_m) ## Get the euclidean distance
+DR <- as.data.frame(as.matrix(DR))
+
+DR2 <- DR %>% mutate(numNA = ncol(DR) - rowMeans(is.na(.))*ncol(DR)) ## find number of NAs per row
+tooManyNAs_R <- unname(which(DR2$numNA <= ncol(DR)-200 )) ## I set it to 200 for a 6000 row matrix but you will have to adjust the parameter
+length(tooManyNAs_R) ## was like 1200 
+
+
+subs_m <- as.matrix(subs)
+subs_m <- subs_m[-c(tooManyNAs_R),] ## Remove rows with too many NAs
+dim(subs_m) ## was like 4500
+
 
