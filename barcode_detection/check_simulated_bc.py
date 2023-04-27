419527699a20fce2675a9527dd1affa5a27be922
Andrey Prjibelski <andrewprzh@gmail.com>
2023-04-27 13:14:08 +0300

    evaluate UMIs too

--- barcode_detection/check_simulated_bc.py
+++ barcode_detection/check_simulated_bc.py
@@ -1,19 +1,33 @@
 import sys
+import editdistance
 # READ_1_ENST00000294189.11_TGACAATCTCCGTA_TACAATAGG_0_aligned_25_R_65_816_74     128     86      95      112     -       TGACAATCTCCGTA  TACAATAGG       14      True
 
 count = 0
 barcoded = 0
 correct = 0
+umis_dists = [0] * 10
+true_umis_dists = [0] * 10
+
 for l in open(sys.argv[1]):
     count += 1
     v = l.split("\t")
     bc = v[6]
+    umi = v[7]
     if bc == "*": continue
     barcoded += 1
 
-    true_bc = v[0].split("_")[3]
+    readv = v[0].split("_")
+    true_bc = readv[3]
+    true_umi = readv[4]
     if true_bc == bc:
         correct += 1
+    umi_ed = editdistance.eval(true_umi, umi)
+    umis_dists[umi_ed] += 1
+    if v[9].startswith("T"):
+        true_umis_dists[umi_ed] += 1
 
 print("Total\t%d\nBarcode\t%d\nCorrect\t%d" % (count, barcoded, correct))
-    
+print("UMIs within 1%d\nUMIs within 2%d\nUMIs within 3%d\n" % (sum(umis_dists[:2]), sum(umis_dists[:3]), sum(umis_dists[:4])))
+print(true_umis_dists)
+print("UMIs within 1%d\nUMIs within 2%d\nUMIs within 3%d\n" % (sum(true_umis_dists[:2]), sum(true_umis_dists[:3]), sum(true_umis_dists[:4])))
+print(true_umis_dists)
