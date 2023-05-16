38971ede8935a199059f8e7f802fbae8c6e2be31
Andrey Prjibelski <andrewprzh@gmail.com>
2023-05-16 18:28:40 +0300

    overlaping transcripts script

---
+++ gff_analysis/gff_transcrip_ovelap.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def load_transcript_ids(inf):
+    transcript_ids = set()
+    for l in open(inf):
+        if l.startswith("#"): continue
+        v = l.strip().split("\t")
+        if len(v) < 9: continue
+        if v[2] != "transcript": continue
+
+        atts = v[8].split(" ")
+        tid = None
+        for i in range(len(atts) - 1):
+            if atts[i] == "transcript_id":
+                tid = atts[i+1][1:-2]
+                break
+        if tid: transcript_ids.add(tid)
+
+    return transcript_ids
+
+
+transcripts1 = load_transcript_ids(sys.argv[1])
+transcripts2 = load_transcript_ids(sys.argv[2])
+
+print("GTF1: %d" % len(transcripts1))
+print("GTF2: %d" % len(transcripts2))
+print("Overlap: %d" % len(transcripts1.intersection(transcripts2)))
