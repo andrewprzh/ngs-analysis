31e124b682ed1be8bf75a0ce7414a1d8b8416753
Andrey Prjibelski <andrewprzh@gmail.com>
2023-06-01 17:35:08 +0300

    merge barcodes

--- barcode_detection/merge_barcodes_illumina.py
+++ barcode_detection/merge_barcodes_illumina.py
@@ -6,11 +6,16 @@ inf2 = open(sys.argv[2])
 while True:
     l1 = inf1.readline()
     l2 = inf2.readline()
+    if not l1 or not l2:
+        break
     v1 = l1.strip().split('\t')
     v2 = l2.strip().split('\t')
+    if len(v1) < 10 or len(v2) < 10:
+        continue
     read1 = v1[0]
     read2 = v2[0]
     assert read2 == read1
+
     bc1 = v1[6]
     umi1 = v1[9]
     bc2 = v2[6]
@@ -23,8 +28,10 @@ while True:
     elif bc2 == "*":
         sys.stdout.write(l1)
     else:
-        sys.stderr.write("Both reads have the barcode")
-        continue
+        if abs(len(umi1) - 9) < abs(len(umi2) - 9):
+            sys.stdout.write(l1)
+        else:
+            sys.stdout.write(l2)
 
 
 
