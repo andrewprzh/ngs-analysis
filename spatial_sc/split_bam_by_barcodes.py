361432c33fd653a686ccbb4847b9452eaa5b4e37
Andrey Prjibelski <andrewprzh@gmail.com>
2023-06-13 16:04:09 +0300

    bam splitter

---
+++ spatial_sc/split_bam_by_barcodes.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import pysam
+from collections import defaultdict
+
+
+def read_called_barcodes(inf):
+    barcoded_reads = {}
+    for l in open(inf):
+        v = l.strip().split('\t')
+        if len(v) < 7:
+            continue
+        bc = v[6]
+        if bc == "*":
+            continue
+        barcoded_reads[v[0]] = v[6]
+    print("Loaded %d read ids" % len(barcoded_reads))
+    return barcoded_reads
+
+
+def filter_reads(in_file_name, out_file_prefix, barcoded_reads, min_reads = 100):
+    inf = pysam.AlignmentFile(in_file_name, "rb")
+    barcode_to_reads = defaultdict(list)
+    count = 0
+    passed = 0
+
+    for read in inf:
+        if read.reference_id == -1:
+            continue
+
+        count += 1
+        if count % 10000 == 0:
+            sys.stdout.write("Processed " + str(count) + " reads\r")
+
+        if read.query_name not in barcoded_reads:
+            continue
+
+        barcode_to_reads[barcoded_reads[read.query_name]].append(read)
+        passed += 1
+
+    print("Processed " + str(count) + " reads, collected " + str(passed))
+    inf.close()
+
+    bc_count = 0
+    for bc in barcode_to_reads.keys():
+        out_file_name = out_file_prefix + bc + ".bam"
+        outf = pysam.AlignmentFile(out_file_name, "wb", template=inf)
+        if len(barcode_to_reads[bc]) < min_reads:
+            continue
+        bc_count += 1
+        for read in barcode_to_reads[bc]:
+            outf.write(read)
+
+        outf.close()
+        pysam.index(out_file_name)
+
+
+if len(sys.argv) < 3:
+    print("Usage: " + sys.argv[0] + " <input.bam> <read list> [prefix]")
+    exit(0)
+
+if len(sys.argv) == 4:
+    prefix = "." + sys.argv[3]
+else:
+    prefix = ""
+
+filter_reads(sys.argv[1], prefix, read_called_barcodes(sys.argv[2]))
