599fd4f9d3f8a528fe1836fa186e844f1e490131
Andrey Prjibelski <andrewprzh@gmail.com>
2023-12-25 17:18:26 +0300

    add gaps to atac filtering

--- spiso-seq/preprocessing/atac_deduplication.py
+++ spiso-seq/preprocessing/atac_deduplication.py
@@ -59,8 +59,47 @@ class IntervalWithAlignments:
         self.end = max(self.end, alignment.reference_end)
         self.alignment_list.append(alignment)
 
+    def non_duplicated(self):
+        return [self.alignment_list[0]]
 
-def filter_atac(bam_file, read2barcode_dict, out_bam):
+
+class SelfFilteringAlignmentStorage:
+    def __init__(self, alignment, start_gap, end_gap):
+        self.start_gap = start_gap
+        self.end_gap = end_gap
+        self.end = alignment.reference_end
+
+        self.chr_id = alignment.reference_name
+        # (alignment, is not duplicated)
+        self.alignment_list = [(alignment, True)]
+
+    def next_alignment_overlaps(self, alignment):
+        # no chance that any of the alignments in the block overlap well because the last one is already too far
+        return (self.chr_id == alignment.reference_name and self.end > alignment.reference_start
+                and abs(self.alignment_list[-1][0].reference_start - alignment.reference_start) <= self.start_gap)
+
+    def extend(self, alignment):
+        for i in range(len(self.alignment_list) - 1, -1, -1):
+            if not self.alignment_list[i][1]:
+                # skip duplicated alignments
+                continue
+            if abs(self.alignment_list[i][0].reference_start - alignment.reference_start) > self.start_gap:
+                # no well overlapping alignments, consider non duplicated
+                self.alignment_list.append((alignment, True))
+                self.end = max(self.end, alignment.reference_end)
+                break
+            if (abs(self.alignment_list[i][0].reference_start - alignment.reference_start) <= self.start_gap and
+                    abs(self.alignment_list[i][0].reference_end - alignment.reference_end) <= self.end_gap):
+                # overlaps a lot, mark as duplicated, do not check further
+                self.alignment_list.append((alignment, False))
+                break
+
+    def non_duplicated(self):
+        for a in self.alignment_list:
+            if a[1]: yield a[0]
+
+
+def filter_atac(bam_file, read2barcode_dict, out_bam, start_gap, end_gap):
     bam = pysam.AlignmentFile(bam_file, "rb")
     outf = pysam.AlignmentFile(out_bam, "wb", template=bam)
     logger.info("Filtering %s, writing to %s" % (bam_file, out_bam))
@@ -79,21 +118,23 @@ def filter_atac(bam_file, read2barcode_dict, out_bam):
         barcoded_reads += 1
         barcode = read2barcode_dict[read_id]
         if barcode not in current_intervals:
-            current_intervals[barcode] = IntervalWithAlignments(a)
+            current_intervals[barcode] = SelfFilteringAlignmentStorage(a, start_gap, end_gap)
             continue
 
         current_interval = current_intervals[barcode]
         if current_interval.next_alignment_overlaps(a):
             current_interval.extend(a)
         else:
-            outf.write(current_interval.alignment_list[0])
-            out_reads += 1
-            current_intervals[barcode] = IntervalWithAlignments(a)
+            for alignment in current_interval.non_duplicated():
+                outf.write(alignment)
+                out_reads += 1
+            current_intervals[barcode] = SelfFilteringAlignmentStorage(a, start_gap, end_gap)
 
     # dump all the remaining alignments
     for current_interval in current_intervals.values():
-        out_reads += 1
-        outf.write(current_interval.alignment_list[0])
+        for alignment in current_interval.non_duplicated():
+            outf.write(alignment)
+            out_reads += 1
 
     logger.info("Done. Processed %d primary alignments (%d barcoded), save %d alignments" % (in_reads, barcoded_reads, out_reads))
     outf.close()
@@ -115,6 +156,10 @@ def parse_args():
     parser.add_argument("--read2barcode", type=str, help="read to barcode table (TSV)", required=True)
     parser.add_argument("--read_column", type=int, default=0, help="read column in read2barcode table [0]")
     parser.add_argument("--barcode_column", type=int, default=1, help="barcode column in read2barcode table [1]")
+    parser.add_argument("--start_gap", type=int, default=10, help="maximal nubmer of bases between "
+                                                                  "starts of duplicated reads [10]")
+    parser.add_argument("--end_gap", type=int, default=10, help="maximal nubmer of bases between "
+                                                                  "ends of duplicated reads [10]")
 
     args = parser.parse_args()
     return args
@@ -125,7 +170,7 @@ def main():
     set_logger(logger)
 
     barcodes = load_barcodes(args.read2barcode, args.read_column, args.barcode_column)
-    filter_atac(args.input, barcodes, args.output)
+    filter_atac(args.input, barcodes, args.output, args.start_gap, args.end_gap)
 
 
 if __name__ == "__main__":
