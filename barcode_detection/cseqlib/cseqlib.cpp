08c197cbbe3aac1a064daa2438517cf97d882214
Andrey Prjibelski <andrewprzh@gmail.com>
2024-02-09 02:14:09 +0200

    inexact matching first version

--- barcode_detection/cseqlib/cseqlib.cpp
+++ barcode_detection/cseqlib/cseqlib.cpp
@@ -244,6 +244,139 @@ public:
 };
 
 
+class InexactKmerIndexer {
+    int w_;
+    int kmer_size_;
+    int seq_counter_;
+    std::vector<std::vector<int>> kmer_index_;
+    size_t mask_;
+    size_t short_mask_;
+
+    size_t shift_nucl(size_t c, size_t kmer_idx) const {
+        kmer_idx <<= 2;
+        kmer_idx &= mask_;
+        return kmer_idx | nuct2bin[c];
+    }
+
+    size_t hash(size_t kmer_idx) const {
+        return (kmer_idx & short_mask_) ^ ((kmer_idx >> (2 * w_)) & short_mask_);
+    }
+
+    void find_by_hash(size_t kmer_idx, std::unordered_map<int, int>& kmer_counts,
+                      std::unordered_map<int, std::vector<int>>& kmer_positions, int& max_count, size_t pos) const {
+        for (const auto idx : kmer_index_[kmer_idx]) {
+            kmer_counts[idx] += 1;
+            kmer_positions[idx].push_back(pos);
+            if (max_count < kmer_counts[idx]) {
+                max_count = kmer_counts[idx];
+            }
+        }
+    }
+
+    void find_with_subs(size_t kmer_idx, std::unordered_map<int, int>& kmer_counts,
+                        std::unordered_map<int, std::vector<int>>& kmer_positions, int& max_count, size_t pos) const {
+        find_by_hash(kmer_idx, kmer_counts, kmer_positions, max_count, pos);
+        for (size_t i = 0; i < w_; ++i) {
+            size_t current_nucl = kmer_idx >> (2 * i) & 3;
+            for (size_t nucl = 0; nucl < 4; ++nucl) {
+                if (nucl == current_nucl) {
+                    continue;
+                }
+                size_t subs_mask = ((1 << (2 * w_)) - 1) ^ (3 << (2 * i));
+                kmer_idx &= subs_mask;
+                kmer_idx |= (nucl << (2 * i));
+                find_by_hash(kmer_idx, kmer_counts, kmer_positions, max_count, pos);
+            }
+        }
+    }
+
+public:
+    InexactKmerIndexer(int w): w_(w), kmer_size_(2 * w), seq_counter_(0), kmer_index_(1 << (2 * w)),
+                             mask_((1 << (2 * kmer_size_)) - 1),  short_mask_((1 << (2 * kmer_size_)) - 1) { }
+
+    void add(const char* seq, int len) {
+        if (len < kmer_size_) return;
+        size_t kmer_idx = 0;
+        for (size_t i = 0; i < kmer_size_; ++i) {
+            kmer_idx |= nuct2bin[seq[i]] << ((kmer_size_ - i - 1) * 2);
+        }
+        size_t kmer_hash = hash(kmer_idx);
+        kmer_index_[kmer_hash].push_back(seq_counter_);
+
+        for (size_t i = kmer_size_; i < len; ++i) {
+            kmer_idx = shift_nucl(seq[i], kmer_idx);
+            kmer_hash = hash(kmer_idx);
+            kmer_index_[kmer_hash].push_back(seq_counter_);
+        }
+        ++seq_counter_;
+    }
+
+    bool empty() const {
+        return seq_counter_ == 0;
+    }
+
+    void get_occurrences(const char* seq, int len, int max_hits, int min_kmers, int hits_delta, int* result, int max_res_size) const {
+        if (len < kmer_size_) return;
+
+        std::unordered_map<int, int> kmer_counts;
+        std::unordered_map<int, std::vector<int>> kmer_positions;
+        int max_count = 0;
+        size_t kmer_idx = 0;
+        for (size_t i = 0; i < kmer_size_; ++i) {
+            kmer_idx |= nuct2bin[seq[i]] << ((kmer_size_ - i - 1) * 2);
+        }
+        size_t kmer_hash = hash(kmer_idx);
+        find_with_subs(kmer_hash, kmer_counts, kmer_positions, max_count, 0);
+
+        for (size_t i = kmer_size_; i < len; ++i) {
+            kmer_idx = shift_nucl(seq[i], kmer_idx);
+            kmer_hash = hash(kmer_idx);
+            find_with_subs(kmer_hash, kmer_counts, kmer_positions, max_count, i);
+            for (const auto idx : kmer_index_[kmer_idx]) {
+                kmer_counts[idx] += 1;
+                kmer_positions[idx].push_back(i - kmer_size_ + 1);
+                if (max_count < kmer_counts[idx]) {
+                    max_count = kmer_counts[idx];
+                }
+            }
+        }
+
+        int count_cutoff = std::max(min_kmers, max_count - hits_delta);
+        std::vector<int> result_seqs;
+        result_seqs.reserve(kmer_counts.size());
+        for (const auto& kmer_count : kmer_counts) {
+            if (kmer_count.second >= count_cutoff) {
+                result_seqs.push_back(kmer_count.first);
+            }
+        }
+        auto& kmer_counts_ = kmer_counts;
+        std::sort(result_seqs.begin(), result_seqs.end(), [&kmer_counts_](int seq_idx1, int seq_idx2) { return kmer_counts_[seq_idx1] < kmer_counts_[seq_idx2]; } );
+
+
+        int top_hits = std::min((int) result_seqs.size(), max_hits);
+        if (max_hits == 0) {
+            top_hits = (int) result_seqs.size();
+        }
+
+        result[0] = top_hits;
+        int current_result_index = 1;
+        for (int i = 0; i < top_hits; ++i) {
+            result[current_result_index++] = result_seqs[i];
+            const auto& positions = kmer_positions[result_seqs[i]];
+            result[current_result_index++] = int(positions.size());
+
+            std::memcpy(result + current_result_index, positions.data(), positions.size() * sizeof(int));
+            current_result_index += positions.size();
+            if (current_result_index >= max_res_size) {
+                break;
+            }
+        }
+
+    }
+
+};
+
+
 
 extern "C" {
 
