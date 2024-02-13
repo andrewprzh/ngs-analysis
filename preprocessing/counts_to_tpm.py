ea881b510521fe4868e92cdab9dc6beb4520db05
Andrey Prjibelski <andrewprzh@gmail.com>
2024-02-13 15:18:40 +0200

    counts2tpm

---
+++ preprocessing/counts_to_tpm.py
@@ -0,0 +1,12 @@
+import sys
+
+count_dict = {}
+for l in open(sys.argv[1]):
+    if l.startswith("#"):
+        continue
+    v = l.split("\t")
+    count_dict[v[0]] = float(v[1])
+
+scale_factor = sum(count_dict.values()) / 1000000.0
+for tid in sorted(count_dict.keys()):
+    print("%s\t%.2f\t%.6f" % (tid, count_dict[tid], count_dict[tid] / scale_factor))
