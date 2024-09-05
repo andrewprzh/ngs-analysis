0ac21985b233f263cfd8f31443de14663e931ef5
Andrey Prjibelski <andrewprzh@gmail.com>
2024-09-05 22:27:22 +0300

    update bam filtering

--- spatial_sc/filter_bam.py
+++ spatial_sc/filter_bam.py
@@ -1,13 +1,30 @@
+#!/usr/bin/env python3
+#
+############################################################################
+# Copyright (c) 2024 University of Helsinki
+# # All Rights Reserved
+# See file LICENSE for details.
+############################################################################
+
+import os
+import sys
+import argparse
+from traceback import print_exc
 import os
 import sys
 import pysam
-import numpy
 
 
-def read_reads(inf):
+def read_reads(inf, col):
     read_set = set()
+    warn_printed = False
     for l in open(inf):
-        read_set.add(l.strip().split('\t')[0])
+        v = l.strip().split('\t')
+        if len(v) > col:
+            read_set.add([col])
+        elif not warn_printed:
+            print("Malformed line with %d columns %s" % (len(v), l))
+            warn_printed = True
     return read_set
 
 
@@ -15,7 +32,7 @@ def read_called_barcodes(inf):
     read_set = set()
     for l in open(inf):
         v = l.strip().split('\t')
-        if len(v) >= 7 and v[6] == "*":
+        if len(v) >= 2 and v[1] == "*":
             continue
         read_set.add(v[0])
     print("Loaded %d read ids" % len(read_set))
@@ -28,14 +45,13 @@ def filter_reads(in_file_name, out_file_name, read_set):
 
     count = 0
     passed = 0
-    lengths = []
 
     for read in inf:
         if read.reference_id == -1 or read.is_secondary:
             continue
 
         count += 1
-        if count % 10000 == 0:
+        if count % 100000 == 0:
             sys.stdout.write("Processed " + str(count) + " reads\r")
 
         if read.query_name in read_set:
@@ -48,12 +64,50 @@ def filter_reads(in_file_name, out_file_name, read_set):
     pysam.index(out_file_name)
 
 
-if len(sys.argv) < 3:
-    print("Usage: " + sys.argv[0] + " <input.bam> <read list> [suffix]")
-    exit(0)
 
-if len(sys.argv) == 4:
-    suffix = "." + sys.argv[3]
-else:
-    suffix = ""
-filter_reads(sys.argv[1], os.path.splitext(sys.argv[1])[0] + ".filtered" + suffix + ".bam", read_called_barcodes(sys.argv[2]))
+
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("--output", "-o", type=str, help="output folder (same as input file if not set), "
+                                                         "or file name")
+    parser.add_argument("--suffix", type=str, help="file suffix (works if output is a folder),"
+                                                   " default: filtered", default="filtered")
+    parser.add_argument("--list", "-l", type=str, help="read list", required=True)
+    parser.add_argument("--read_column", type=int, help="read id column (0 based index)", default=0)
+    parser.add_argument("--barcodes", default=False, action="store_true",
+                        help="read list is barcode files, filter only barcoded ones")
+
+    parser.add_argument("--bam", "-b", type=str, help="BAM file ", required=True)
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    in_bam = args.bam
+    if not args.output:
+        args.output = os.path.dirname(in_bam)
+
+    if os.path.isdir(args.output):
+        out_bam = os.path.join(args.output, os.path.splitext(os.path.basename(in_bam))[0] + "." + args.suffix + ".bam")
+    else:
+        out_bam = args.output
+
+    if args.barcodes:
+        read_set = read_called_barcodes(args.list)
+    else:
+        read_set = read_reads(args.list, args.read_column)
+
+    filter_reads(in_bam, out_bam, read_set)
+
+
+if __name__ == "__main__":
+   # stuff only to run when not called via 'import' here
+    try:
+        main()
+    except SystemExit:
+        raise
+    except:
+        print_exc()
+        sys.exit(-1)
