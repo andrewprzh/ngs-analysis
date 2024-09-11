2167ea48dbb295776b2cfc34c422fe45750e52ec
Andrey Prjibelski <andrewprzh@gmail.com>
2024-09-11 11:22:05 +0300

    bam stats wip

--- spatial_sc/bam_stats.py
+++ spatial_sc/bam_stats.py
@@ -31,10 +31,94 @@ def has_intron(cigartules):
     return any(x[0] == 3 for x in cigartules)
 
 
+
+def get_read_blocks(ref_start, cigar_tuples):
+    read_pos = 0
+    ref_pos = ref_start + 1
+    cigar_index = 0
+    current_ref_block_start = None
+    current_read_block_start = None
+    current_cigar_block_start = None
+    has_match = False
+    ref_blocks = []
+    cigar_blocks = []
+    read_blocks = []
+
+    while cigar_index < len(cigar_tuples):
+        cigar_event = CigarEvent(cigar_tuples[cigar_index][0])
+        event_len = cigar_tuples[cigar_index][1]
+
+        if current_ref_block_start is None and cigar_event in CigarEvent.get_ins_del_match_events():
+            # init new block from match
+            current_ref_block_start = ref_pos
+            current_read_block_start = read_pos
+            current_cigar_block_start = cigar_index
+            if cigar_event == CigarEvent.insertion:
+                read_pos += event_len
+            elif cigar_event == CigarEvent.deletion:
+                ref_pos += event_len
+            else:
+                read_pos += event_len
+                ref_pos += event_len
+                has_match = True
+        # found intron, add current block
+        elif cigar_event in CigarEvent.get_match_events():
+            read_pos += event_len
+            ref_pos += event_len
+            has_match = True
+        elif cigar_event == CigarEvent.insertion:
+            read_pos += event_len
+        elif cigar_event == CigarEvent.deletion:
+            ref_pos += event_len
+        elif cigar_event == CigarEvent.skipped:
+            if current_ref_block_start:
+                if has_match:
+                    ref_blocks.append((current_ref_block_start, ref_pos - 1))
+                    read_blocks.append((current_read_block_start, read_pos - 1))
+                    cigar_blocks.append((current_cigar_block_start, cigar_index - 1))
+                has_match = False
+                current_ref_block_start = None
+                current_read_block_start = None
+                current_cigar_block_start = None
+            ref_pos += event_len
+        elif cigar_event == CigarEvent.soft_clipping:
+            if current_ref_block_start:
+                if has_match:
+                    ref_blocks.append((current_ref_block_start, ref_pos - 1))
+                    read_blocks.append((current_read_block_start, read_pos - 1))
+                    cigar_blocks.append((current_cigar_block_start, cigar_index - 1))
+                has_match = False
+                current_ref_block_start = None
+                current_read_block_start = None
+                current_cigar_block_start = None
+            read_pos += event_len
+
+        cigar_index += 1
+
+    if current_ref_block_start and has_match:
+        ref_blocks.append((current_ref_block_start, ref_pos - 1))
+        read_blocks.append((current_read_block_start, read_pos - 1))
+        cigar_blocks.append((current_cigar_block_start, cigar_index - 1))
+
+    return ref_blocks, read_blocks, cigar_blocks
+
+
+def correct_bam_coords(blocks):
+    return list(map(lambda x: (x[0] + 1, x[1]), blocks))
+
+
+
+def introns_known(read, intron_chr_set):
+    chr_id = read.reference_name
+
+
+
 def count_stats(in_file_name, barcode_dict=None, intron_chr_set=None):
     inf = pysam.AlignmentFile(in_file_name, "rb")
     total_reads = 0
     stat_dict = defaultdict(int)
+    intron_barcode_dict = defaultdict(int)
+    known_intron_barcode_dict  = defaultdict(int)
 
     for read in inf:
         total_reads += 1
@@ -51,10 +135,13 @@ def count_stats(in_file_name, barcode_dict=None, intron_chr_set=None):
         else:
             barcoded = "no barcode"
 
-        if has_intron(read.cigartuples):
+        is_spliced = has_intron(read.cigartuples)
+        if is_spliced:
             spliced = "spliced"
         else:
             spliced = "unspliced"
+
+
 
         stat_dict[(mapped, barcoded, spliced)] += 1
 
@@ -72,6 +159,7 @@ def parse_args():
     # parser.add_argument("--output", "-o", type=str, help="output folder (same as input file if not set), "
     #                                                      "or file name")
     parser.add_argument("--barcodes", type=str, help="read barcodes")
+    parser.add_argument("--genedb", type=str, help="gffutils genedb (for intron stats)")
     parser.add_argument("--bam", "-b", nargs="+", type=str, help="BAM file ", required=True)
 
     args = parser.parse_args()
