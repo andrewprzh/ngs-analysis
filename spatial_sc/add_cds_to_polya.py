b75716d469a1ee2250daa59007255ebdbf742487
Andrey Prjibelski <andrewprzh@gmail.com>
2024-10-01 17:53:48 +0300

    UTR

--- spatial_sc/add_cds_to_polya.py
+++ spatial_sc/add_cds_to_polya.py
@@ -15,17 +15,56 @@ from collections import defaultdict
 import gffutils
 
 
+def interval_len(interval):
+    return interval[1] - interval[0] + 1
 
-def find_cds(gffutils_db, t_id):
+
+def intervals_total_length(sorted_range_list):
+    total_len = 0
+    for r in sorted_range_list:
+        total_len += interval_len(r)
+    return total_len
+
+
+def find_cds(gffutils_db, t_id, polya_pos):
     t = gffutils_db[t_id]
     cds = []
-    for e in gffutils_db.children(t, featuretype="CDS"):
+    exons = []
+    for e in gffutils_db.children(t, featuretype="CDS", order_by="start"):
         cds.append((e.start, e.end))
+    for e in gffutils_db.children(t, featuretype="exons", order_by="start"):
+        cds.append((e.start, e.end))
+
     if not cds:
         return "NC"
 
-    cds_end_pos = cds[-1][1] if t.strand == "+" else cds[0][0]
-    return "%s_%d_%d_%s" % (t.seqid, cds_end_pos, cds_end_pos, t.strand)
+    utr = []
+    if t.strand == "+":
+        cds_end_pos = cds[-1][1]
+        for i in range(len(exons)):
+            e = exons[i]
+            if e[0] <= cds_end_pos <= e[1]:
+                utr.append((cds_end_pos + 1, e[1]))
+            elif cds_end_pos <= e[0]:
+                utr.append(e)
+
+        utr[-1] = (utr[-1][0], polya_pos)
+    else:
+        cds_end_pos = cds[0][0]
+        for i in range(len(exons)):
+            e = exons[i]
+            if e[0] <= cds_end_pos <= e[1]:
+                utr.append((e[0], cds_end_pos - 1))
+                break
+            elif e[1] <= cds_end_pos:
+                utr.append(e)
+
+        utr[0] = (polya_pos, utr[0][1])
+        
+    utr = sorted(utr)
+    utr_str = ";%;" + ";%;".join("%s_%d_%d_%s" % (t.seqid, e[0], e[1], t.strand) for e in utr)
+
+    return "%s_%d_%d_%s" % (t.seqid, cds_end_pos, cds_end_pos, t.strand), str(intervals_total_length(utr)), utr_str
 
 
 def parse_args():
@@ -48,13 +87,15 @@ def main():
                 outf.write(l)
                 continue
             l = l.strip()
-            t_id = l.split('\t')[1]
+            v = l.strip().split('\t')
+            t_id = v[1]
+            polya_pos = int(v[4].split("_")[1])
             if t_id in cds_dict:
-                cds = cds_dict[t_id]
+                cds, utr_len, utrs = cds_dict[t_id]
             else:
-                cds = find_cds(gffutils_db, t_id)
-                cds_dict[t_id] = cds
-            outf.write(l + "\t" + str(cds) + "\n")
+                cds, utr_len, utrs = find_cds(gffutils_db, t_id, polya_pos)
+                cds_dict[t_id] = (cds, utr_len, utrs)
+            outf.write(l + "\t%s\t%s\t%s\n" % (cds, utr_len, utrs))
 
 
 
