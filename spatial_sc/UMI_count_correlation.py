fe22de76a58ea0bae141e182308d809edec2f15a
Andrey Prjibelski <andrewprzh@gmail.com>
2024-10-14 18:09:59 +0300

    add chrM filtering

--- spatial_sc/UMI_count_correlation.py
+++ spatial_sc/UMI_count_correlation.py
@@ -10,15 +10,24 @@ import sys
 import argparse
 from traceback import print_exc
 from collections import defaultdict
+import pysam
 
 # 82718f26-b81f-4561-ac75-59b987941431    ACGCGTTTAAGACG  ATCGGCTAG       14      True    -       82      40      49      66
 # LH00376:28:22GVKLLT3:4:1101:2655:1016   41      -1      8       25      +       CAACTGGCCGGGTA  TACCGTCGT       13      False
 
-def get_umi_dict(inf, trusted_only=False):
+def get_umi_dict(inf, bam=None, trusted_only=False):
+    forbidden_reads = set()
+    if bam:
+        for r in pysam.AlignmentFile(bam, "rb").fetch("chrM"):
+            forbidden_reads.add(r.query_name)
+
     umi_dict = defaultdict(set)
     for l in open(inf):
         if l.startswith("#"): continue
         v = l.strip().split('\t')
+        read_id = v[0]
+        if read_id in forbidden_reads:
+            continue
         if v[-1] in ['True', 'False']:
             # old format
             if v[6] == '*' or v[7] == '*': continue
@@ -37,6 +46,7 @@ def parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("--output", "-o", type=str, help="output file with UMI count tables", required=True)
     parser.add_argument("--input", "-i", nargs="+", type=str, help="input TSV(s) with barcoded reads", required=True)
+    parser.add_argument("--bam", "-b", nargs="+", type=str, help="BAM(s) for filtering out chrM, same as barcoded reads (optional)")
     parser.add_argument("--only_trusted_umi", default=False, action="store_true", help="keep only spliced reads")
 
     args = parser.parse_args()
@@ -45,10 +55,15 @@ def parse_args():
 
 def main():
     args = parse_args()
+
+    if args.bam and len(args.bam) != len(args.input):
+        print("Unequal number of TSV and BAM files")
+        exit(-1)
+
     dicts = []
-    for tsv in args.input:
+    for i, tsv in enumerate(args.input):
         print("Loading %s" % tsv)
-        dicts.append(get_umi_dict(tsv, args.only_trusted_umi))
+        dicts.append(get_umi_dict(tsv, args.bam[i], args.only_trusted_umi))
 
     print("Merging barcodes")
     all_barcodes = set()
