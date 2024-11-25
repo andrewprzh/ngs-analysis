b6b143628fa7356be56c7bdcd7efe11eaa2e9568
Andrey Prjibelski <andrewprzh@gmail.com>
2024-11-25 17:46:03 +0200

    polya filter

--- spatial_sc/check_exon_polya_tests.py
+++ spatial_sc/check_exon_polya_tests.py
@@ -12,7 +12,7 @@ import argparse
 import gffutils
 from traceback import print_exc
 from collections import defaultdict
-
+import numpy
 
 DELTA = 0
 
@@ -67,28 +67,40 @@ def get_transcripts(genedb, gene):
 
 def process_test(polya1, polya2, exon, transcripts, strand):
     combinations = set()
+    # print(polya1, polya2, exon, strand)
+    diff = abs(polya1 - polya2)
+    p1_a = False
+    p2_a = False
     for t in transcripts:
         exons = transcripts[t]
+        # print(exons, strand)
         polya_site = exons[-1][1] if strand == '+' else exons[0][0]
-        has_polya1 = equal_pos(polya1, polya_site, DELTA)
-        has_polya2 = equal_pos(polya2, polya_site, DELTA)
+        has_polya1 = equal_pos(polya1, polya_site, 50)
+        has_polya2 = equal_pos(polya2, polya_site, 50)
+        if polya1 == polya_site: p1_a = True
+        if polya2 == polya_site: p2_a = True
         if not has_polya1 and not has_polya2: continue
 
         contains_exon = any(equal_ranges(e, exon, DELTA) for e in exons)
         if has_polya1:
             combinations.add((polya1, contains_exon))
-        elif has_polya2:
+        if has_polya2:
             combinations.add((polya2, contains_exon))
 
-    return len(combinations)
+#    print("Combinations %d %s" % (len(combinations), str(combinations)))
+#    if len(combinations) < 2 or (len(combinations) == 2 and len(set([x[0] for x in combinations])) == 1):
+#        print("+++ ODD POLYA! +++")
+    return len(combinations), diff, p1_a, p2_a
 
 
 # chr2_152694389_152694389_-,chr2_152672551_152672551_-,chr2_152714550_152714629_ENSG00000196504.19_-
 def process_csv(inf, genedb, gene_id_dict):
     combination_stats = defaultdict(int)
+    close_polya = defaultdict(int)
+    diffs = defaultdict(int)
     for l in open(inf):
         if l.startswith("PolyA"): continue
-        v = l.split(',')
+        v = l.strip().split(',')
         assert len(v) == 3
         polya1 = int(v[0].split('_')[1])
         polya2 = int(v[1].split('_')[1])
@@ -96,8 +108,24 @@ def process_csv(inf, genedb, gene_id_dict):
         exon = (int(exon_values[1]), int(exon_values[2]))
         gene_id = gene_id_dict[exon_values[3].split('.')[0]]
         strand = exon_values[4]
+
         transcripts = get_transcripts(genedb, gene_id)
-        combination_stats[process_test(polya1, polya2, exon, transcripts, strand)] += 1
+        #print("=== %s ===" % gene_id)
+        comb, diff, p1_a, p2_a = process_test(polya1, polya2, exon, transcripts, strand)
+        if comb == 4 and diff >= 10:
+            print(l.strip())
+            diffs[diff] += 1
+        for c in [1, 3, 6, 10, 20, 50]:
+            if diff <= c:
+                close_polya[(c, p1_a and p2_a)] += 1
+        combination_stats[comb] += 1
+        #print("<<< %s >>>" % gene_id)
+
+    #print(close_polya)
+
+#    print(sum(diffs.values()))
+#    for k in sorted(diffs.keys()):
+#        print(k, diffs[k])
     return combination_stats
 
 
@@ -113,10 +141,11 @@ def parse_args():
 
 def main():
     args = parse_args()
+    global DELTA
     DELTA = args.delta
     genedb = gffutils.FeatureDB(args.genedb)
     gene_ids = get_geneid_map(genedb)
-    print(process_csv(args.csv, genedb, gene_ids))
+    process_csv(args.csv, genedb, gene_ids)
 
 
 if __name__ == "__main__":
